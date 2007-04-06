e43f787bcc5a1cc3ddd8ba37132e3d04841b4477
TParker <TParker@leapfrog.com>
2007-04-06 11:18:21 -0700

    SVN SUCKS!
    (SVN Revision 121 from svn://localhost/TRUNK/)

---
+++ System/Event/PublicMPI/EventListener.cpp
@@ -0,0 +1,77 @@
+//============================================================================
+// $Source: $
+//
+// Copyright (c) LeapFrog Enterprises, Inc.
+//============================================================================
+//
+// File:
+//		EventListener.cpp
+//
+// Description:
+//		Implements the underlying Event Manager module.
+//
+//============================================================================
+
+#include <Module.h>
+#include <EventListener.h>
+#include <EventListenerPriv.h>
+#include <EventPriv.h>
+
+
+//============================================================================
+// IEventListener
+//============================================================================
+//----------------------------------------------------------------------------
+IEventListener::IEventListener(const tEventType* pTypes, U32 count)
+	: mpimpl(NULL)
+{
+	ICoreModule*	pModule;
+	Module::Connect(pModule, kEventModuleName, kEventModuleVersion);
+	CEventModule* pEvent = reinterpret_cast<CEventModule*>(pModule);
+	if( pEvent )
+	{
+		mpimpl = pEvent->GenerateEventListenerImpl(pTypes, count);
+		Module::Disconnect(pModule);
+	}
+}
+
+//----------------------------------------------------------------------------
+IEventListener::~IEventListener()
+{
+	ICoreModule*	pModule;
+	Module::Connect(pModule, kEventModuleName, kEventModuleVersion);
+	CEventModule* pEvent = reinterpret_cast<CEventModule*>(pModule);
+	if( pEvent )
+	{
+		pEvent->UnregisterEventListener(this);
+		Module::Disconnect(pModule);
+		delete mpimpl;
+	}
+}
+
+//----------------------------------------------------------------------------
+const IEventListener* IEventListener::GetNextListener() const
+{
+	return mpimpl->GetNextListener();
+}
+
+//----------------------------------------------------------------------------
+tErrType IEventListener::SetNextListener(const IEventListener* pListener)
+{
+	return mpimpl->SetNextListener(pListener);
+}
+
+//----------------------------------------------------------------------------
+tErrType IEventListener::DisableNotifyForEventType(tEventType type)
+{
+	return mpimpl->DisableNotifyForEventType(type);
+}
+
+//----------------------------------------------------------------------------
+tErrType IEventListener::ReenableNotifyForEventType(tEventType type)
+{
+	return mpimpl->ReenableNotifyForEventType(type);
+}
+
+
+// EOF
---
+++ System/Event/PublicMPI/EventMPI.cpp
@@ -0,0 +1,120 @@
+//============================================================================
+// $Source: $
+//
+// Copyright (c) LeapFrog Enterprises, Inc.
+//============================================================================
+//
+// File:
+//		Event.cpp
+//
+// Description:
+//		Implements the Module Public Interface (MPI) for the Brio 
+//		Event Manager module.
+//
+//============================================================================
+
+#include <StringTypes.h>
+#include <SystemErrors.h>
+
+#include <EventMPI.h>
+#include <EventPriv.h>
+#include <Module.h>
+
+
+const tVersion	kMPIVersion = MakeVersion(0,1);
+const CString	kMPIName = "EventMPI";
+
+
+//============================================================================
+//----------------------------------------------------------------------------
+CEventMPI::CEventMPI() : mpModule(NULL)
+{
+	ICoreModule*	pModule;
+	Module::Connect(pModule, kEventModuleName, kEventModuleVersion);
+	mpModule = reinterpret_cast<CEventModule*>(pModule);
+}
+
+//----------------------------------------------------------------------------
+CEventMPI::~CEventMPI()
+{
+	Module::Disconnect(mpModule);
+}
+
+//----------------------------------------------------------------------------
+Boolean	CEventMPI::IsValid() const
+{
+	return (mpModule != NULL) ? true : false;
+}
+
+//----------------------------------------------------------------------------
+tErrType CEventMPI::GetMPIVersion(tVersion &version) const
+{
+	version = kMPIVersion;
+	return kNoErr;
+}
+
+//----------------------------------------------------------------------------
+tErrType CEventMPI::GetMPIName(ConstPtrCString &pName) const
+{
+	pName = &kMPIName;
+	return kNoErr;
+}
+
+//----------------------------------------------------------------------------
+tErrType CEventMPI::GetModuleVersion(tVersion &version) const
+{
+	if(!mpModule)
+		return kMpiNotConnectedErr;
+	return mpModule->GetModuleVersion(version);
+}
+
+//----------------------------------------------------------------------------
+tErrType CEventMPI::GetModuleName(ConstPtrCString &pName) const
+{
+	if(!mpModule)
+		return kMpiNotConnectedErr;
+	return mpModule->GetModuleName(pName);
+}
+
+//----------------------------------------------------------------------------
+tErrType CEventMPI::GetModuleOrigin(ConstPtrCURI &pURI) const
+{
+	if(!mpModule)
+		return kMpiNotConnectedErr;
+	return mpModule->GetModuleOrigin(pURI);
+}
+
+
+//============================================================================
+//----------------------------------------------------------------------------
+tErrType CEventMPI::RegisterEventListener(const IEventListener *pListener,
+											tEventRegistrationFlags flags)
+{
+	if(!mpModule)
+		return kMpiNotConnectedErr;
+	return mpModule->RegisterEventListener(pListener, flags);
+}
+
+//----------------------------------------------------------------------------
+tErrType CEventMPI::UnregisterEventListener(const IEventListener *pListener)
+{
+	if(!mpModule)
+		return kMpiNotConnectedErr;
+	return mpModule->UnregisterEventListener(pListener);
+}
+
+
+
+//============================================================================
+//----------------------------------------------------------------------------
+tErrType CEventMPI::PostEvent(const IEventMessage &msg, 
+								tEventPriority priority, 
+								const IEventListener *pListener) const
+{
+	if(!mpModule)
+		return kMpiNotConnectedErr;
+	return mpModule->PostEvent(msg, priority, pListener);
+}
+
+
+// EOF
