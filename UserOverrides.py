722e4c4d8a72a29fd850aedd22d8a6292b652172
TParker <TParker@leapfrog.com>
2007-04-12 21:49:11 -0700

    Added comments
    (SVN Revision 143 from svn://localhost/TRUNK/)

--- UserOverrides.py
+++ UserOverrides.py
@@ -6,6 +6,10 @@ import re
 
 #-----------------------------------------------------------------------------
 # Override settings in the construction environment object
+# Provide a single function here for platform, and modify the construction
+# environment for that platform.  Using Python string parsing and list 
+# manipulations, you can add, remove and modify the switches sent to the 
+# compiler
 #-----------------------------------------------------------------------------
 def LightningGCC_embedded(env):
 	env['CCFLAGS']		+= ''
