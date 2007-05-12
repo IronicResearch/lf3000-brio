a7d4cb13e9215909697c242e19368caa1254d79a
BKuznetsov <BKuznetsov@leapfrog.com>
2007-05-11 17:52:22 -0700

    Interim checkin
    (SVN Revision 240 from svn://localhost/TRUNK/)

--- System/Kernel/Kernel.cpp
+++ System/Kernel/Kernel.cpp
@@ -22,7 +22,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <assert.h>
-
+#include <stdarg.h>
 
 #include <SystemTypes.h>
 #include <SystemErrors.h>
@@ -31,6 +31,13 @@
 #include <KernelMPI.h>
 #include <KernelPriv.h>
 
+#include <signal.h>
+
+extern "C"
+{
+	void sig_handler(int signal, siginfo_t *psigInfo, void *pFunc);
+	void *sig_handler_timer_task(void *parm);
+}
 //==============================================================================
 // Defines
 //==============================================================================
@@ -44,7 +51,14 @@ const CURI	kModuleURI	= "/LF/System/Kernel";
 // single instance of module object.
 static CKernelModule*	sinst = NULL;
 
-
+//==============================================================================
+// Definition variables for timer 
+//==============================================================================
+#define NUMBERMESSAGES 10
+#define POLLINGTIME 10000000   
+static pthread_t pthreadTimer = 0; 
+static unsigned bufferTimer[NUMBERMESSAGES];
+static int counterCreatedTimers = 0;
 //==============================================================================
 // Internal untility functions
 //==============================================================================
@@ -173,17 +187,18 @@ tErrType CKernelModule::CreateTask(tTaskHndl& hndl,
 	ASSERT_POSIX_CALL(err);
 	
 	pthread_t	pthread;
-    err = pthread_create(&pthread,
+   	err = pthread_create(&pthread,
                      &tattr,
                      (void*(*)(void*))props.TaskMainFcn,
                      (void *)props.pTaskMainArgValues);
+
     ASSERT_POSIX_CALL(err);
     hndl = static_cast<tTaskHndl>(pthread);
     // FIXME/tp: Determine if 0 is a valid value for pthread, and hav the "hndl" always
     // represent the pthread+1 if so.
 
 	err = pthread_attr_destroy(&tattr);
-	ASSERT_POSIX_CALL(err);
+	ASSERT_POSIX_CALL(errno);
 	
 	// good to go
 //	*pHndl = pTask->ToOpaqueType();
@@ -199,7 +214,15 @@ tErrType CKernelModule::CreateTask(tTaskHndl& hndl,
 //------------------------------------------------------------------------------
 tErrType CKernelModule::JoinTask( tTaskHndl pHndl, tPtr& threadReturnValue )
 {
-	return AsBrioErr(pthread_join(pHndl, &threadReturnValue));  
+//	return AsBrioErr(pthread_join(pHndl, &threadReturnValue));
+	tErrType err = kNoErr;
+	
+	errno = 0;
+	err = pthread_join(pHndl, &threadReturnValue);
+	ASSERT_POSIX_CALL(errno);
+	
+    return kNoErr;
+  
 }
 
 //------------------------------------------------------------------------------
@@ -209,11 +232,15 @@ tErrType CKernelModule::CancelTask( tTaskHndl hndl )
 // NOTE 1: A terminated task cannot execute again until it is reset.
 // NOTE 2: When calling this function from a signal handler, the task
 //         whose signal handler is executing cannot be terminated.
-	return AsBrioErr(pthread_cancel(hndl));
+//	return AsBrioErr(pthread_cancel(hndl));
+	tErrType err = kNoErr;
+	
+	errno = 0;
+	err = pthread_cancel(hndl);
+	ASSERT_POSIX_CALL(errno);
+	
+    return kNoErr;
 }
-
-
-
 //------------------------------------------------------------------------------
 // This service returns the currently active task pointer.
 tTaskHndl CKernelModule::GetCurrentTask() const
@@ -251,14 +278,17 @@ int CKernelModule::GetTaskSchedulingPolicy(tTaskHndl hndl) const
 //------------------------------------------------------------------------------
 void CKernelModule::TaskSleep(U32 msec) const
 {
-    while( msec > 999 )     /* For OpenBSD and IRIX, argument */
-    {                       /* to usleep must be < 1000000.   */
-        usleep( 999000 );
-        msec -= 999;
-	}
-    usleep( msec * 1000 );
+ //   while( msec > 999 )     /* For OpenBSD and IRIX, argument */
+ //   {                       /* to usleep must be < 1000000.   */
+ //       usleep( 999000 );
+ //       msec -= 999;
+//	}
+//    usleep( msec * 1000 ); This function is obsolete. Need to use nanosleep instead.
+	struct timespec sleeptime;
+	sleeptime.tv_sec = msec /1000;
+	sleeptime.tv_nsec = (msec % 1000) * 1000000;
+	nanosleep( &sleeptime, NULL ); 
 }
-
 
 //============================================================================
 // Allocating memory from the System heap
@@ -277,7 +307,6 @@ tErrType CKernelModule::Free(tPtr ptr)
 	free(ptr);
 	return kNoErr;
 }
-
 
 //============================================================================
 // Terminal/debug IO
@@ -318,27 +347,33 @@ tErrType CKernelModule::CreateMessageQueue(tMessageQueueHndl& hndl,
     queuAttr.mq_msgsize	= props.mq_msgsize;
     queuAttr.mq_curmsgs	= props.mq_curmsgs;
     
-#if 1 // BSK Debug Printing
+#if 0 // BSK Debug Printing
     printf("THe Input MUEUE parameters are :\n"); 
-    printf("name=%s oflag=%d mode=%d mq_flags=%d mq_maxmsg=%d mq_msgsize=%d mq_curmsgs=%d\n",
-    	props.nameQueue, props.oflag,props.mode,
+    printf("name=%s oflag=%d mode=%d \n mq_flags=%d mq_maxmsg=%d mq_msgsize=%d mq_curmsgs=%d\n",
+    	props.nameQueue, props.oflag, props.mode,
     	queuAttr.mq_flags, props.mq_maxmsg, props.mq_msgsize, props.mq_curmsgs);   
     fflush(stdout);
 #endif // BSK
     
     errno = 0;
     
-    retMq_open = mq_open (props.nameQueue,
-                          props.oflag,
-                          props.mode,
+    retMq_open = mq_open (props.nameQueue, // /test_q
+                          props.oflag,     // 578
+                          props.mode,      // 448
                           &queuAttr);
     
-	ASSERT_POSIX_CALL(err);
+	ASSERT_POSIX_CALL(errno);
     hndl = retMq_open;
     
-#if 1 // BSK Debug Printing
+#if 0 // BSK Debug Printing
     printf("Return value of mq_open function is %d ERRNO=%d\n", retMq_open, errno);
     fflush(stdout);;
+//         struct mq_attr {
+//							long mq_flags;       /* Flags: 0 or O_NONBLOCK */
+//							long mq_maxmsg;      /* Max. # of messages on queue */
+//	            			long mq_msgsize;     /* Max. message size (bytes) */
+//             				long mq_curmsgs;     /* # of messages currently in queue */
+//         				  }
     
     struct mq_attr attr = {0};
     mq_getattr(retMq_open, &attr);
@@ -363,8 +398,11 @@ tErrType CKernelModule::DestroyMessageQueue(tMessageQueueHndl hndl)
 	// FIXME/tp: Need ot fold UnlinkMessageQueue() into this function to truly
 	// destroy the queue.  This will require tracking its name through its handle.
     errno = 0;
-    mq_close(hndl);
-    ASSERT_POSIX_CALL( errno );
+    if( hndl != 0 )
+    {
+    	mq_close(hndl);
+    	ASSERT_POSIX_CALL( errno );
+    }
     return kNoErr;
 }
 
@@ -439,7 +477,7 @@ tErrType CKernelModule::SendMessage(tMessageQueueHndl hndl, const CMessage& msg)
     
     ASSERT_POSIX_CALL( errno );
     
-#if 1 // BSK Debugging printing 
+#if 0 // BSK Debugging printing 
     struct mq_attr attr = {0};
     mq_getattr(hndl, &attr);
     printf("\n-----After sending message QUEQUE attributes are: ----\n");
@@ -482,7 +520,7 @@ tErrType CKernelModule::SendMessageOrWait(tMessageQueueHndl hndl,
     
    	ASSERT_POSIX_CALL( errno );
     
-#if 1 // Debugging printing 
+#if 0 // Debugging printing 
     struct mq_attr attr = {0};
     mq_getattr(hndl, &attr);
     printf("\n-----After sending message QUEQUE attributes are: ----\n");
@@ -510,7 +548,7 @@ tErrType CKernelModule::ReceiveMessage( tMessageQueueHndl hndl,
    struct mq_attr attr = {0};
    unsigned int  msg_prio;
    
-#if 1 // BSK Debug printing
+#if 0 // BSK Debug printing
    U8 tmp = msg_ptr->GetMessagePriority();
    mq_getattr(hndl, &attr);
    printf("\n-----CKernelModule::ReceiveMessage ----\n");
@@ -541,7 +579,7 @@ tErrType CKernelModule::ReceiveMessageOrWait( tMessageQueueHndl hndl,
 // Retrieve attributes of the messges queque
    struct mq_attr attr = {0};
    
-#if 1 // BSK Debug printing
+#if 0 // BSK Debug printing
    mq_getattr(hndl, &attr);
    printf("\n-----CKernelModule::ReceiveMessage ----\n");
    printf("mq_flags=%d  mq_maxmsg=%d mq_msgsize=%d mq_curmsq=%d\n",
@@ -566,7 +604,7 @@ tErrType CKernelModule::ReceiveMessageOrWait( tMessageQueueHndl hndl,
                                   &msg_prio,
                                   (const struct timespec *)&tp);
 	ASSERT_POSIX_CALL( errno );
-     assert(msg_prio == msg_ptr->GetMessagePriority());
+    assert(msg_prio == msg_ptr->GetMessagePriority());
 
 #if 1 // BSK Debug printing
     printf("CKernelModule::ReceiveMessage. Received=%d bytes\n", ret_receive);
@@ -586,29 +624,88 @@ U32   		CKernelModule::GetElapsedTime( U32* pUs )
 	gettimeofday( &time, NULL );
 	if( pUs )
 		*pUs = time.tv_usec;
-	return (time.tv_sec * 1000 );
+	return ( time.tv_usec / 1000 );
 }
 
 //------------------------------------------------------------------------------
-tTimerHndl CKernelModule::CreateTimer(tCond& condition, 
-									const tTimerProperties& props,
-									const char* /*pDebugName*/ )
+tErrType CKernelModule::CreateTimer(tTimerHndl& hndl, pfnTimerCallback callback,
+ 						tTimerProperties& props, const char* pDebugName )
 {
-//	tErrType err = timer_create(CLOCK_REALTIME, NULL, (timer_t *)pHndl);
-//	tErrType err = timer_create(CLOCK_MONOTONIC, NULL, (timer_t *)pHndl);
-//	tErrType err = timer_create(CLOCK_THREAD_CPUTIME_ID, NULL, (timer_t *)pHndl);
-	
-	struct sigevent se;
+	tErrType err = kNoErr;
+    sigset_t signal_set;
+
+//	clockid_t clockid;     Now, it is used CLOCK_REALTIME
+						// There are the following possible values:
+						//	CLOCK_MONOTONIC
+						//	CLOCK_THREAD_CPUTIME_ID
+	clockid_t clockid = CLOCK_REALTIME;
+//	clockid = CLOCK_REALTIME;
+						// struct sigevent se;
+						// Event structure
+	                    // The  evp  argument,  if  non-NULL,  points  to  a sigevent structure.
+	                    // This structure, allocated by the application,
+       					// defines the asynchronous notification to occur as specified  in  Signal  Generation
+       					// and  Delivery  when  the  timer expires.  If the evp argument is NULL, the effect
+       					// is as if the evp argument pointed to a sigevent structure with the sigev_notify
+       					// member having the value  SIGEV_SIGNAL,  the  sigev_signo  having  a  default
+       					// signal  number,  and  the sigev_value member having the value of the timer ID.
+               		    //--------------------------------------------------------------------------------
+               		    //     	union sigval
+               		    //		 {    /* Data passed with notification */
+             			//				int     sival_int;   /* Integer value */
+             			//				void   *sival_ptr;   /* Pointer value */
+         				//		  };
+                        // 
+         				//		struct sigevent 
+         				//		{
+             			//			int    sigev_notify;      /* Notification method */
+             			//			int    sigev_signo;       /* Notification signal */
+             			//			union sigval sigev_value; /* Data passed with notification */
+             			//			void (*sigev_notify_function) (union sigval);
+                        //       							/* Function for thread notification */
+             			//			void  *sigev_notify_attributes;	/* Thread function attributes */
+         				//		};
+
+    // Create new task to process timer budder 
+	if( 0 == pthreadTimer )
+	{
+		   pthread_create(&pthreadTimer, NULL,
+    						sig_handler_timer_task, NULL);
+    	   ASSERT_POSIX_CALL(errno);
+	}	
+
+    int signum = SIGRTMAX;     
+
+    /* Set up signal handler */
+    struct sigaction act;
+    sigfillset( &act.sa_mask );
+    act.sa_flags = SA_SIGINFO; //SA_RESTART| // 
+    act.sa_sigaction = sig_handler;
+//    act.sa_handler = sig_handler; 
+    sigaction( signum, &act, NULL ); 
+    pthread_sigmask( SIG_BLOCK, &signal_set, NULL );
+
+	// Set up timer
+    struct sigevent se;
+    memset(&se, 0, sizeof(se)); 
+	se.sigev_notify = SIGEV_SIGNAL;
+	se.sigev_signo = signum;
+//	se.sigev_value.sival_int = 334455;
+	se.sigev_value.sival_ptr = (void *)callback;
+
+//	se.sigev_value.sival_int = 0;
+//	se.sigev_notify_attributes = (void *)callback;
 	timer_t	posixHndl;
+
     errno = 0;
-    timer_create(CLOCK_REALTIME, &se, &posixHndl);
-    
+    timer_create(clockid, &se, &posixHndl); // BSK
 	ASSERT_POSIX_CALL( errno );
+	counterCreatedTimers++;
+	hndl = AsBrioTimerHandle(posixHndl);
 	
-	tTimerHndl hndl = AsBrioTimerHandle(posixHndl);
-	ResetTimer(hndl, props);
-    
-    return hndl; 
+//	ResetTimer(hndl, props);
+
+    return kNoErr;
 }
 
 //------------------------------------------------------------------------------
@@ -616,10 +713,15 @@ tErrType CKernelModule::DestroyTimer( tTimerHndl hndl )
 {
 	
     errno = 0;
-    timer_delete(AsPosixTimerHandle(hndl));
+    timer_delete(AsPosixTimerHandle( hndl ));
     
 	ASSERT_POSIX_CALL( errno );
-    
+
+	if( --counterCreatedTimers <= 0 )
+	{
+		pthread_cancel( pthreadTimer );
+		ASSERT_POSIX_CALL( errno );
+	};
     return kNoErr; 
 }
 	
@@ -628,13 +730,13 @@ tErrType CKernelModule::ResetTimer(tTimerHndl hndl, const tTimerProperties& prop
 { 
 	struct itimerspec value;
 	
-    value.it_interval.tv_sec = 0;
-	value.it_interval.tv_nsec = (long )props.dummy;
-	value.it_value.tv_sec = 0;
-	value.it_value.tv_nsec = 0;
+    value.it_interval.tv_sec = props.timeout.it_interval.tv_sec;
+	value.it_interval.tv_nsec = props.timeout.it_interval.tv_nsec;
+	value.it_value.tv_sec = props.timeout.it_value.tv_sec;
+	value.it_value.tv_nsec = props.timeout.it_value.tv_nsec;
 	
     errno = 0;
-    timer_settime(AsPosixTimerHandle(hndl), 0, &value, NULL );
+    timer_settime(AsPosixTimerHandle( hndl ), props.type, &value, NULL );
     
 	ASSERT_POSIX_CALL( errno );
 	// FIXME/tp: Need to internally track the sigevent and clock the condition
@@ -645,11 +747,12 @@ tErrType CKernelModule::ResetTimer(tTimerHndl hndl, const tTimerProperties& prop
 //------------------------------------------------------------------------------
 tErrType CKernelModule::StartTimer( tTimerHndl hndl )
 {
+
 	struct itimerspec value;
 	// FIXME/tp: Lookup timer properties stored in ResetTimer and initialize "value"
 
     errno = 0;
-    timer_settime(AsPosixTimerHandle(hndl), 0, &value, NULL );
+    timer_settime(AsPosixTimerHandle( hndl ), 0, &value, NULL );
     
 	ASSERT_POSIX_CALL( errno );
     return kNoErr; 
@@ -665,8 +768,10 @@ tErrType CKernelModule::StopTimer( tTimerHndl hndl )
 	value.it_value.tv_sec = 0;
 	value.it_value.tv_nsec = 0;
 	
+	U32 remaining = GetTimerRemainingTime(hndl);
+	
     errno = 0;
-    timer_settime(AsPosixTimerHandle(hndl), 0, &value, NULL );	
+    timer_settime(AsPosixTimerHandle( hndl ), 0, &value, NULL );	
 	
     ASSERT_POSIX_CALL( errno );
 
@@ -683,7 +788,7 @@ U32 CKernelModule::GetTimerElapsedTime( tTimerHndl hndl ) const
 //	fflush(stdout);
 	
     errno = 0;
-    int err = timer_gettime( AsPosixTimerHandle(hndl), &value );
+    int err = timer_gettime( AsPosixTimerHandle( hndl ), &value );
 	
 	ASSERT_POSIX_CALL( errno );
 	
@@ -706,7 +811,7 @@ U32 CKernelModule::GetTimerRemainingTime( tTimerHndl hndl ) const
 	struct itimerspec value;
 	
     errno = 0;
-    timer_gettime( AsPosixTimerHandle(hndl), &value );
+    timer_gettime( AsPosixTimerHandle( hndl ), &value );
 	
     ASSERT_POSIX_CALL( errno );
 	
@@ -743,8 +848,82 @@ extern "C"
 		delete sinst;
 		sinst = NULL;
 	}
+
+void sig_handler( int signal, siginfo_t *psigInfo, void *pFunc)
+{
+	for(int i = 0; i < NUMBERMESSAGES; i++)
+	{
+		if( 0 == bufferTimer[ i ] )
+		{
+			bufferTimer[ i ] = (unsigned )psigInfo->si_value.sival_ptr;
+#if 0 // FIXME/BSK
+			printf("psigInfo->si_errno sival_ptr=0X%x\n",
+		 	psigInfo->si_value.sival_ptr);
+			fflush(stdout);	
+#endif
+			break; 	
+		}
+	}	
 }
 
+void *sig_handler_timer_task(void *parm)
+{
+	struct timespec sleeptime = {0, POLLINGTIME};        
+
+	while( 1 )
+	{
+		for(int i = 0; i < NUMBERMESSAGES; i++)
+		{
+			if( 0 != bufferTimer[ i ] )
+			{
+				pfnTimerCallback callback =
+				        reinterpret_cast<pfnTimerCallback>(bufferTimer[ i ]);
+				bufferTimer[ i ] = 0; 	
+				(*callback)();
+#if 0 // FIXME/BSK
+				printf("callback 0x%x\n", callback);
+#endif				
+			}
+		}
+		nanosleep( &sleeptime, NULL );
+	}
+}	
+
+#if 0  // FIXME/BSK
+		sigset_t signal_set;
+		int sig;
+	
+		printf("Hello Boris\n");
+		fflush(stdout);	
+        
+        while (1)
+        { 
+			sigfillset(&signal_set);
+			sigwait( &signal_set, &sig );
+			switch( sig )
+            {
+                /* whatever you need to do on SIGQUIT */
+			// FIXME/BSK
+               	case SIGALRM:  
+						printf("Hello Boris\n");
+						fflush( stdout );
+                  break;
+                /* whatever you need to do on SIGQUIT */
+                	case SIGQUIT:  
+                  break;
+
+                /* whatever you need to do on SIGINT */
+                 	case SIGINT: 
+                  break;
+
+                /* whatever you need to do for other signals */
+                	default:   
+                  break;
+                }
+        }        
+	}
+#endif
+} // extern "C"
 
 // EOF
 
