3b62b992d7bceb719b75d070c2f499e32061a231
BKuznetsov <BKuznetsov@leapfrog.com>
2007-05-11 17:54:28 -0700

    Interim checkin
    (SVN Revision 241 from svn://localhost/TRUNK/)

--- System/Kernel/tests/TestKernel.h
+++ System/Kernel/tests/TestKernel.h
@@ -4,6 +4,7 @@
 #include <StringTypes.h>
 #include <KernelMPI.h>
 #include <UnitTestUtils.h>
+#include <CoreTypes.h> 
 
 #include <signal.h>   	// for timer_create
 #include <time.h> 		// for timer create
@@ -12,36 +13,54 @@
 using namespace std;
 LF_USING_BRIO_NAMESPACE()
 
+
+//---------------------------------------------------------
+// Task testing fuctions
+//----------------------------------------------------------
 typedef struct{
 	int numTest;
-	char *testDescription;
+	char testDescription[128];
 }thread_arg_t;
 
-    void *myTask(void* threadArg)
-    {
-		printf("myTask was created and started\n");
-	
-		int my_policy;
-		struct sched_param my_param;
-        thread_arg_t *sentData;
+static char *message[3] = { "Properties are default",
+					 "Properties are set - Test 2",
+					 "Properties are set - Test 3"
+};						 
 
-		sentData = (thread_arg_t *)threadArg;
-		printf("----Test=#%d %s \n", sentData->numTest, sentData->testDescription);   
-		
-		pthread_getschedparam(pthread_self(),
-						  &my_policy,
-						  &my_param);
-		
-		printf("\nThread routine running at %s Priority is %d\n",
-		(my_policy == SCHED_FIFO ? "FIFO"
-		:(my_policy == SCHED_RR ? "RR"
-		:(my_policy == SCHED_OTHER ? "OTHER"
-		:"unknown"))),
-		my_param.sched_priority);						   	
-		fflush( stdout );
-		
-		sleep(2);	// In order to test Cancel function
-    }
+static void *myTask(void* arg)
+{
+	thread_arg_t *ptr = (thread_arg_t *)arg; 
+	TS_ASSERT( !strcmp(message[ ptr->numTest ], ptr->testDescription ) );
+	sleep(1); 
+}	
+//---------------------------------------------------------
+// Timer testing fuctions
+//----------------------------------------------------------
+static tTimerHndl hndlTimer_1;
+static tTimerHndl hndlTimer_2;
+static tTimerHndl hndlTimer_3;
+
+	static void myTask_Timer_1()
+	{
+		// FIXME /BSK
+//		printf("myTask_Timer_1 - The timer called me\n");
+//		fflush(stdout);
+	}	
+
+	static void myTask_Timer_2()
+	{
+		// FIXME /BSK
+//		printf("myTask_Timer_2 - The timer called me\n");
+//		fflush(stdout);
+	}	
+
+	static void myTask_Timer_3()
+	{
+		// FIXME /BSK
+//		printf("myTask_Timer_3 - The timer called me\n");
+//		fflush(stdout);
+	}	
+//----------------------------------------------------------
 //============================================================================
 // TestAudioMgr functions
 //============================================================================
@@ -55,8 +74,7 @@ public:
 	{
 		KernelMPI = new CKernelMPI();
 	}
-
-	//------------------------------------------------------------------------
+//------------------------------------------------------------------------
 	void tearDown( )
 	{
 		delete KernelMPI; 
@@ -88,58 +106,89 @@ public:
 	
 	void testCreateTask()
 	{
-//--------------------------------------------------------------		
-// 	For reference:
-//		struct tTaskProperties 
-//		{
-//			U32 				priority;	            // 1
-//			tAddr				stackAddr;				// 2
-//			U32 				stackSize;				// 3	
-//			tTaskMainFcn_posix	TaskMainFcn;            // 4
-//			U32					taskMainArgCount;       // 5
-//			tPtr				pTaskMainArgValues;		// 6						
-//			tTaskStartupMode	startupMode;	        // 7
-//			tTaskSchedPolicy	schedulingPolicy;		// 8
-//			U32					schedulingInterval;     // 9
-//   		int                 inheritSched;           // 10
-//		};
-//----------------------------------------------------------------
-
 		const CURI *pTaskURI = NULL;
-		tTaskProperties pProperties;
-		tTaskHndl *pHndl;
+		tTaskHndl pHndl_1;
+		tTaskHndl pHndl_2;
+		tTaskHndl pHndl_3;
         thread_arg_t threadArg;
-        int testNumber = 1;
- #if 0
- //-----------------	Test 1	----------------------------        
-        threadArg.numTest = testNumber++;
-        char *message1 = "Default Properties";
-        threadArg.testDescription = message1;
+        int testNumber = 0;
+ 	    tTaskProperties pProperties;
+        tPtr status = NULL;
+/*
+ struct tTaskProperties {
+	U32 				priority;	            // 1
+	tAddr				stackAddr;				// 2
+	U32 				stackSize;				// 3	
+	tTaskMainFcn_posix	TaskMainFcn;            // 4
+	U32					taskMainArgCount;       // 5
+	tPtr				pTaskMainArgValues;		// 6						
+	tTaskStartupMode	startupMode;	        // 7
+	tTaskSchedPolicy	schedulingPolicy;		// 8
+	U32					schedulingInterval;     // 9
+    int                 inheritSched;           // 10
+ */
+ 
+//-----------------	Test 1	----------------------------        
+        char *message1 = "Properties are default";
 
 		pProperties.TaskMainFcn = (void* (*)(void*))myTask;
+    	strcpy(threadArg.testDescription, message[ testNumber ] );
+		threadArg.numTest = testNumber++;
 		pProperties.pTaskMainArgValues = &threadArg;
 		
-		TS_ASSERT_EQUALS( kNoErr, KernelMPI->CreateTask( NULL, &pProperties, pHndl) );
-		TS_ASSERT_EQUALS( kNoErr, KernelMPI->JoiningThreads( *pHndl, NULL ) );
+		TS_ASSERT_EQUALS( kNoErr,KernelMPI->CreateTask( pHndl_1,
+		 	    		 (const tTaskProperties )pProperties, NULL) );
+
+		TS_ASSERT_EQUALS( pProperties.priority, KernelMPI->GetTaskPriority( pHndl_1 ) ); 
+		TS_ASSERT_EQUALS( pProperties.schedulingPolicy, KernelMPI->GetTaskSchedulingPolicy( pHndl_1 ) ); 
+
+		TS_ASSERT_EQUALS( kNoErr, KernelMPI->JoinTask( pHndl_1, status));
 
 //-----------------	Test 2	----------------------------        
-        threadArg.numTest = testNumber++;
-        char *message3 = "Set Priority and Policcy";
-        threadArg.testDescription = message3;
+    char *message2 = "Properties are set";
+ // 	priority;				// 1	
+	pProperties.priority = 50;
+ 
+ //	stackAddr;				// 2
+	const unsigned PTHREAD_STACK_ADDRESS = 0xABC;
+//	pProperties.stackAddr = (tAddr ) malloc(PTHREAD_STACK_ADDRESS + 0x4000);
 
-		pProperties.priority = 50;
-    	pProperties.schedulingPolicy = SCHED_FIFO;
+//	stackSize;				// 3	
+    const unsigned PTHREAD_STACK_MINIM = 0x10000;
+//	pProperties.stackSize = PTHREAD_STACK_MIN + 0x4000;
 
-        pProperties.TaskMainFcn = (void* (*)(void*))myTask;
-		pProperties.pTaskMainArgValues = &threadArg;
-		
-		TS_ASSERT_EQUALS( kNoErr, KernelMPI->CreateTask( NULL, &pProperties, pHndl) );
-		TS_ASSERT_EQUALS( kNoErr, KernelMPI->JoiningThreads( *pHndl, NULL ) );
+//	TaskMainFcn;			// 4
+	pProperties.TaskMainFcn = (void* (*)(void*))myTask;
+
+//	taskMainArgCount & pTaskMainArgValues		// 5 & 6
+    	strcpy(threadArg.testDescription, message[ testNumber ] );
+		threadArg.numTest = testNumber++;
+
+//	startupMode;			// 7
+
+//	pProperties.startupMode 
+
+//	schedulingPolicy;		// 8
+    pProperties.schedulingPolicy = SCHED_FIFO;
+
+//	schedulingInterval;		// 9
+//  pProperties.schedulingInterval 
+		TS_ASSERT_EQUALS( kNoErr,KernelMPI->CreateTask( pHndl_2,
+		 	    		 (const tTaskProperties )pProperties, NULL) );
+
+		TS_ASSERT_EQUALS( pProperties.priority, KernelMPI->GetTaskPriority( pHndl_2 ) ); 
+		TS_ASSERT_EQUALS( pProperties.schedulingPolicy, KernelMPI->GetTaskSchedulingPolicy( pHndl_2 ) ); 
+
+		TS_ASSERT_EQUALS( kNoErr, KernelMPI->JoinTask( pHndl_2, status));
+
+
 //-----------------	Test 3	----------------------------        
+	    char *message3 = "Properties are set";
 
-        threadArg.numTest = testNumber++;
-        char *message2 = "Cancel Thread";
-        threadArg.testDescription = message2;
+    	strcpy(threadArg.testDescription, message[ testNumber ] );
+		threadArg.numTest = testNumber++;
+
+		pProperties.pTaskMainArgValues = &threadArg;
 
 		pProperties.priority = 50;
     	pProperties.schedulingPolicy = SCHED_FIFO;
@@ -147,36 +196,184 @@ public:
         pProperties.TaskMainFcn = (void* (*)(void*))myTask;
 		pProperties.pTaskMainArgValues = &threadArg;
 		
-		TS_ASSERT_EQUALS( kNoErr, KernelMPI->CreateTask( NULL, &pProperties, pHndl) );
-		TS_ASSERT_EQUALS( kNoErr, KernelMPI->CancelTask( *pHndl ) );
-#endif
-//-----------------	Test 4	----------------------------        
+		TS_ASSERT_EQUALS( kNoErr,KernelMPI->CreateTask( pHndl_3,
+		 	    		 (const tTaskProperties )pProperties, NULL) );
+
+		TS_ASSERT_EQUALS( pProperties.priority, KernelMPI->GetTaskPriority( pHndl_3 ) ); 
+		TS_ASSERT_EQUALS( pProperties.schedulingPolicy, KernelMPI->GetTaskSchedulingPolicy( pHndl_3 ) ); 
+
+		TS_ASSERT_EQUALS( kNoErr, KernelMPI->JoinTask( pHndl_3, status));
+
+		TS_ASSERT_EQUALS( kNoErr, KernelMPI->CancelTask( pHndl_1 ) );
+		TS_ASSERT_EQUALS( kNoErr, KernelMPI->CancelTask( pHndl_2 ) );
+		TS_ASSERT_EQUALS( kNoErr, KernelMPI->CancelTask( pHndl_3 ) );
+	}
+
+	void testTaskSleep()
+	{
+		U32 msec = 50;
+		U32 threshold = 10;
+		struct timespec t1, t2;
+		int dt_msec;
+
+		clock_gettime( CLOCK_REALTIME, &t1) ; 
+		KernelMPI->TaskSleep( msec );	
+		clock_gettime( CLOCK_REALTIME, &t1) ; 
+		dt_msec = t2.tv_sec * 1000 + t1.tv_nsec / 1000000 - 
+			     (t2.tv_sec * 1000 + t1.tv_nsec / 1000000);
+			      
+ 		TS_ASSERT_LESS_THAN(dt_msec, msec + threshold); 
+	}
+			
+	
+	void testMemory()
+	{
         U32 size = 0x5000;
         tPtr pPtr = NULL;
         
+//        TS_WARN("TODO: Test Memory!");
         pPtr = KernelMPI->Malloc(size);
 //		TS_ASSERT_DIFFERS( pPtr, static_cast<tPtr>(kNull) );
 //		TS_ASSERT_EQUALS( kNoErr, KernelMPI->Free( pPtr ) );
         
   		TS_ASSERT_DIFFERS( (void*)NULL, pPtr );
   		TS_ASSERT_EQUALS( kNoErr, KernelMPI->Free( pPtr ) );
+	}
+		
+	void TestCreateMessageQueue_1() 
+	{
+		tErrType err;
+		tMessageQueueHndl hndl;
+		
+		const tMessageQueuePropertiesPosix msgProperties = 
+		{
+    		0,                          // msgProperties.blockingPolicy;  
+    		"/test_q_1",                  // msgProperties.nameQueue
+    		S_IRWXU,                    // msgProperties.mode 
+    		O_RDWR|O_CREAT|O_TRUNC,       // msgProperties.oflag  
+    		0,                          // msgProperties.priority
+    		0,                          // msgProperties.mq_flags
+    		10,                          // msgProperties.mq_maxmsg
+    		sizeof(CEventMessage),      // msgProperties.mq_msgsize
+    		0                           // msgProperties.mq_curmsgs
+		};
 
+//        TS_WARN("TODO: Test Create/Destroy Message Queue_1!");
+		err = KernelMPI->CreateMessageQueue(hndl,
+							msgProperties,
+							(const char* )NULL );
+		TS_ASSERT_EQUALS( kNoErr, err );							
+		if( !err )
+		{
+			err = KernelMPI->DestroyMessageQueue( hndl );
+			TS_ASSERT_EQUALS( kNoErr, err );
+			err = KernelMPI->UnlinkMessageQueue( msgProperties.nameQueue );
+			TS_ASSERT_EQUALS( kNoErr, err );
+		}
+	}
+			
+	void TestCreateMessageQueue_2() 
+	{
+		tErrType err;
+		tMessageQueueHndl hndl;
+		
+		const tMessageQueuePropertiesPosix msgProperties = 
+		{
+    		0,                          // msgProperties.blockingPolicy;  
+    		"/test_q_2",                // msgProperties.nameQueue
+    		S_IRWXU,                    // msgProperties.mode 
+    		O_RDWR|O_CREAT|O_TRUNC,     // msgProperties.oflag  
+    		0,                          // msgProperties.priority
+    		0,                          // msgProperties.mq_flags
+    		10,                         // msgProperties.mq_maxmsg
+    		sizeof(CEventMessage)/2,    // msgProperties.mq_msgsize
+    		0                           // msgProperties.mq_curmsgs
+		};
 
+//        TS_WARN("TODO: Test Create/Destroy Message Queue_2!");
+		err = KernelMPI->CreateMessageQueue(hndl,
+							msgProperties,
+							(const char* )NULL );
+		TS_ASSERT_EQUALS( kNoErr, err );							
+		if( !err )
+		{
+			err = KernelMPI->DestroyMessageQueue( hndl );
+			TS_ASSERT_EQUALS( kNoErr, err );
+			err = KernelMPI->UnlinkMessageQueue( msgProperties.nameQueue );
+			TS_ASSERT_EQUALS( kNoErr, err );
+		}								
+	}
 
-//-----------------	Test 5	----------------------------        
-// Testing timer functions	
-// Prototype
-// 	tErrType CreateTimer(tTimerGenHndl& hndl, pfnTimerCallback callback,
-// 						tTimerProperties& props, const char* pDebugName = NULL );
-//
-//
-/*
+	void testCreateTimer()
+	{
+//TS_WARN("TODO: Test Create/Destroy Timer!");
         int signum = SIGRTMAX;
-		tTimerProperties props = {SIGEV_SIGNAL,SIGRTMAX, CLOCK_REALTIME};
+		tTimerProperties props = {0};
+		pfnTimerCallback callback;
 		
-		tTimerHndl hndl = KernelMPI->CreateTimer( NULL, & props, NULL );
-		TS_ASSERT_DIFFERS( hndl, static_cast<hndl>(kNull) );
-*/
+		props.type = TIMER_ABSTIME_SET;
+
+		callback = myTask_Timer_1; // (void (*)())NULL
+		KernelMPI->CreateTimer(hndlTimer_1, callback, props, (const char *)0 );
+		TS_ASSERT_DIFFERS( 0, hndlTimer_1 );
+
+		callback = myTask_Timer_2; // (void (*)())NULL
+		KernelMPI->CreateTimer(hndlTimer_2, callback, props, (const char *)0 );
+		TS_ASSERT_DIFFERS( 0, hndlTimer_2 );
+
+		callback = myTask_Timer_3; // (void (*)())NULL
+		KernelMPI->CreateTimer(hndlTimer_3, callback, props, (const char *)0 );
+		TS_ASSERT_DIFFERS( 0, hndlTimer_3 );
+
+//		TS_ASSERT_DIFFERS( hndl, static_cast<hndl>( kNull ));
+//		tErrType err = KernelMPI->DestroyTimer( hndlTimer );
+//		TS_ASSERT_EQUALS( err, ((tErrType)0) );
+	}
+
+	void testResetTimerRelative()
+	{
+//        TS_WARN("TODO: Test Reset Timer Relative!");
+		tTimerProperties props = {0};
+		tErrType err;
+		props.type = TIMER_RELATIVE_SET; 	
+		// Timer period
+    	props.timeout.it_interval.tv_sec = 1;
+		props.timeout.it_interval.tv_nsec = 500000000L;
+		// Timer expirated
+		props.timeout.it_value.tv_sec = 1;
+		props.timeout.it_value.tv_nsec = 0;
+		err = KernelMPI->ResetTimer( hndlTimer_1, props );
+		TS_ASSERT_EQUALS( err, ((tErrType)0) );
+
+		err = KernelMPI->ResetTimer( hndlTimer_2, props );
+		TS_ASSERT_EQUALS( err, ((tErrType)0) );
+
+		err = KernelMPI->ResetTimer( hndlTimer_3, props );
+		TS_ASSERT_EQUALS( err, ((tErrType)0) );
+		sleep(10);
+	}
+
+    void testDestroyTimer()
+    {
+//        TS_WARN("TODO: Test Destroy Timer!");
+		sleep( 2 );
+		tErrType err;
+		if( hndlTimer_1 != 0 )
+		{	
+			err = KernelMPI->DestroyTimer( hndlTimer_1 );
+			TS_ASSERT_EQUALS( err, ((tErrType)0) );
+		}
 		
-	}		
+		if( hndlTimer_1 != 0 )
+		{
+			err = KernelMPI->DestroyTimer( hndlTimer_2 );
+			TS_ASSERT_EQUALS( err, ((tErrType)0) );
+		}
+		if( hndlTimer_1 != 0 )
+		{
+			err = KernelMPI->DestroyTimer( hndlTimer_3 );
+			TS_ASSERT_EQUALS( err, ((tErrType)0) );
+		}
+    }
+
 };
