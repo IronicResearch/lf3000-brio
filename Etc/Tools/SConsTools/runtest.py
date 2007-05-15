e7367c08f267bddabfb3e92d9e86a115eb97406d
BCavagnolo <BCavagnolo@leapfrog.com>
2007-05-15 13:52:01 -0700

    Added a simple ButtonDemo to the Samples.  It kinda works:) Fixed EXTRA_LINUX_HEADER_DIR problem by using getenv First pass implementation of Button Handler
    (SVN Revision 283 from svn://localhost/TRUNK/)

--- Etc/Tools/SConsTools/runtest.py
+++ Etc/Tools/SConsTools/runtest.py
@@ -12,6 +12,7 @@ import os
 def generate(env):
     
     def builder_unit_test(target, source, env):
+		# TODO: BC: This fails for embedded target because test can't be run.
         app = str(source[0].abspath)
         if os.spawnl(os.P_WAIT, app, app)==0:
             open(str(target[0]),'w').write("PASSED\n")
--- System/Button/Lightning/ButtonHandler.cpp
+++ System/Button/Lightning/ButtonHandler.cpp
@@ -17,6 +17,12 @@
 #include <KernelMPI.h>
 #include <KernelTypes.h>
 #include <SystemErrors.h>
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include "buttons.h"
+
 LF_BEGIN_BRIO_NAMESPACE()
 
 
@@ -28,6 +34,9 @@ namespace
 {
 	//------------------------------------------------------------------------
 	U32			gLastState;
+	int button_fd;
+	struct button_event current_be;
+	CDebugMPI	dbg(kGroupButton);
 }
 
 
@@ -36,19 +45,21 @@ namespace
 // Asynchronous notifications
 //============================================================================
 //----------------------------------------------------------------------------
-static void* TempButtonTask(void*)
+static void* LighteningButtonTask(void*)
 {
-	CDebugMPI	dbg(kGroupButton);
-	dbg.DebugOut(kDbgLvlVerbose, "TempButtonTask: Started\n");
+	CEventMPI eventmgr;
+	dbg.DebugOut(kDbgLvlVerbose, "%s: Started\n", __FUNCTION__);
+	
+	while(1) {
 
-	CEventMPI		eventmgr;
-	tButtonData		data;
- 	data.buttonState = data.buttonTransition = 0;
+		int size = read(button_fd, &current_be, sizeof(struct button_event));
+		dbg.DebugOut(kDbgLvlVerbose, "Read data.\n");
+		dbg.Assert( size >= 0, "button read failed" );
+		
+		tButtonData	data = { current_be.button, current_be.type };		
+		CButtonMessage msg(data);
+		eventmgr.PostEvent(msg, kButtonEventPriority);
 
-	for( bool bDone = false; !bDone; )	// FIXME/tp: when break out???
-	{
-;//			CButtonMessage	msg(data);
-//			eventmgr.PostEvent(msg, kButtonEventPriority);
 	}
 	return NULL;
 }
@@ -56,7 +67,25 @@ static void* TempButtonTask(void*)
 //----------------------------------------------------------------------------
 void CButtonModule::InitModule()
 {
-	//TODO: Setup interrupt handler here
+	tErrType	status = kModuleLoadFail;
+	CKernelMPI	kernel;
+	dbg.SetDebugLevel(kDbgLvlVerbose);
+
+	if( kernel.IsValid() )
+	{
+		tTaskHndl handle;
+		tTaskProperties	properties;
+		properties.pTaskMainArgValues = NULL;
+		properties.TaskMainFcn = LighteningButtonTask;
+		status = kernel.CreateTask(handle, properties);
+	}
+	dbg.Assert( status == kNoErr, 
+				"Lightening Button InitModule: background task creation failed" );
+
+
+	button_fd = open( "/dev/gpio", O_RDWR);
+	dbg.Assert(button_fd != -1, "Lightening Button: cannot open /dev/gpio");
+
 }
 
 
@@ -65,11 +94,10 @@ void CButtonModule::InitModule()
 //============================================================================
 //----------------------------------------------------------------------------
 tButtonData CButtonModule::GetButtonState() const
-//tErrType CButtonModule::GetButtonState(tButtonData& data) const
 {
 	tButtonData	data = { 0, 0 };
 	data.buttonState = data.buttonTransition = 0;
-	return data; //kNoErr;
+	return data;
 }
 
 LF_END_BRIO_NAMESPACE()
