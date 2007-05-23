6cfee55dfb7def093e8dc26b708d3a16735cb066
BKuznetsov <BKuznetsov@leapfrog.com>
2007-05-23 15:57:35 -0700

    Return to the previous version
    (SVN Revision 376 from svn://localhost/TRUNK/)

--- System/Include/KernelMPI.h
+++ System/Include/KernelMPI.h
@@ -37,109 +37,73 @@ public:
 	//==============================================================================
 	// Tasks
 	//==============================================================================
-	// Creates a new thread which starts executing at the routine function
 	tErrType	CreateTask( tTaskHndl& hndl, 		// FIXME/tp: return handle?
 							const tTaskProperties& properties,
 							const char* pDebugName = NULL );
-	
-	// Suspends execution of the calling thread until the target thread
-	// completes its execution.
     tErrType	JoinTask( tTaskHndl hndl, tPtr& threadReturnValue );
-	
-	// Requests that thread be canceled
 	tErrType	CancelTask( tTaskHndl hndl );
 	
-	// Returns the thread ID of the calling thread
 	tTaskHndl	GetCurrentTask() const;
-	
-	// Gets the task priority of the thread given by thread.
 	int			GetTaskPriority( tTaskHndl hndl ) const;
-	
-	// Gets the scheduling parameter of the thread given by thread.
 	int			GetTaskSchedulingPolicy( tTaskHndl hndl ) const;
 
-	// Causes the caller to sleep for at least the amount of time specified in duration
 	void	 	TaskSleep( U32 msec ) const;
 	
 	//==============================================================================
 	// Memory Allocation
 	//==============================================================================
-	// Allocates size bytes and returns a pointer to the allocated memory
 	tPtr		Malloc( U32 size );
-	
-	// Frees the memory space pointed to by pAllocatedMemory, which has been returned
-	// by a previous call to Malloc
 	tErrType	Free( tPtr pAllocatedMemory );
 
 	//==============================================================================
 	// Message Queues
 	//==============================================================================
 	// FIXME/tp: Non-posix-specific props
-	// Associates a message queue descriptor with the message queue specified by name. name
-	// uniquely identifies the message queue across the system.
 	tErrType	OpenMessageQueue( tMessageQueueHndl& hndl,
 									const tMessageQueuePropertiesPosix& props,
 									const char* pDebugName = NULL ); 
-	
-	// Closes the message queue, invalidating hndl
 	tErrType	CloseMessageQueue( tMessageQueueHndl hndl,
 	 								const tMessageQueuePropertiesPosix& props );
 	
-	// Clears message Queue
+	// FIXME/tp: Need to fold unlink into destroy
+	// FIXME/BSK Delete
+	// tErrType	UnlinkMessageQueue( const char *name );
+
 	tErrType  	ClearMessageQueue( tMessageQueueHndl hndl );
 
-	// Gets the the current number of messages.
 	int		 	GetMessageQueueNumMessages( tMessageQueueHndl hndl ) const;
     
-	// Adds the message pointed to by hndl to the message queue
 	tErrType	SendMessage( tMessageQueueHndl hndl, const CMessage& msg );
-	
-	// behaves just like SendMessage(), except that if the queue is full and the O_NONBLOCK flag
-	// is not enabled for the message queue description, then timeoutMs specifies a ceiling on the time
-	// for which the call will block
 	tErrType  	SendMessageOrWait( tMessageQueueHndl hndl, const CMessage& msg, 
 									U32 timeoutMs = kMaxTimeoutMs );
 
 	// FIXME/tp: Work these and incorporate with IEventMessage
 	// FIXME/tp: Think through order of parameters
-	// behaves just like ReceiveMessage(), except that if the queue is empty and the O_NONBLOCK flag
-	// is not enabled for the message queue description, then timeoutMs specifies a ceiling on the time
-	// for which the call will block
+	tErrType  	ReceiveMessage( tMessageQueueHndl hndl, CMessage* msg, 
+									U32 maxMessageSize );
 	tErrType  	ReceiveMessageOrWait( tMessageQueueHndl hndl, CMessage* msg,  
 									U32 maxMessageSize, U32 timeoutMs = kMaxTimeoutMs );
 				
 	//==============================================================================
 	// Time & TimerstTimerGenHndl
 	//==============================================================================
-	// Returns time as millisecond and microseconds since the Epoch
 	U32 	GetElapsedTime( U32* pUs=NULL ) const;	// elapsed time since System startup 
-	
-	// Creates a per-process timer using CLOCK_REALTIME as a base.
+//	U32			GetElapsedTimeInMilliSec( ) const;	// elapsed time since System startup 
+// fixme/BSK
 	tTimerHndl 	CreateTimer( pfnTimerCallback callback, const tTimerProperties& props,
 								const char* pDebugName = NULL );
     
-	// Deletes the timer created by CreateTimer()
     tErrType 	DestroyTimer( tTimerHndl hndl );
 
-	// Resets timer
-	tErrType 	ResetTimer( tTimerHndl hndl, const tTimerProperties& props );
+/**/tErrType 	ResetTimer( tTimerHndl hndl, const tTimerProperties& props );
 
-	// Start the timer with parameters defined in the tTimerProperties property
 	tErrType	StartTimer( tTimerHndl hndl, const tTimerProperties& props  );
-	
-	// Turns off the timer
-	tErrType	StopTimer( tTimerHndl hndl );
+/**/tErrType	StopTimer( tTimerHndl hndl );
 
-	// Pauses the timer
 	tErrType	PauseTimer( tTimerHndl hndl, saveTimerSettings& saveValue );
-	
-	// Resume the timer
-	ttErrType	ResumeTimer( tTimerHndl hndl, saveTimerSettings& saveValue );
+	tErrType	ResumeTimer( tTimerHndl hndl, saveTimerSettings& saveValue );
 
-	// Returns the amount of time after the last timer expiration as millisecond and microseconds
 	U32			GetTimerElapsedTime( tTimerHndl hndl, U32* pUs=NULL ) const;
-	
-	// Returns the amount of time before the timer expires as millisecond and microseconds
 	U32			GetTimerRemainingTime( tTimerHndl hndl, U32* pUs=NULL ) const;
 
 	//==============================================================================
