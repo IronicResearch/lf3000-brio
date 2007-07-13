1205ff0b435f670f4971e91a0960f5d8203870ef
GKlimanis <GKlimanis@leapfrog.com>
2007-07-13 16:26:12 -0700

    Many updates, including sampling rate conversion, FIR filtering in floating point and fixed point.
    (SVN Revision 895 from svn://localhost/TRUNK/)

--- System/Audio/DspUtil/fir.h
+++ System/Audio/DspUtil/fir.h
@@ -24,8 +24,21 @@ extern "C" {
 #define kFIR_Type_BandPass	2
 #define kFIR_Type_BandStop	3
 
-#define kFIR_Type_HalfBand	10
-#define kFIR_Type_ThirdBand	11
+// 1/2 Band filters  (fs/4)
+#define kFIR_Type_HalfBand_15	10
+#define kFIR_Type_HalfBand_31	11
+#define kFIR_Type_HalfBand_32dB	12
+#define kFIR_Type_HalfBand_58dB	13
+#define kFIR_Type_HalfBand	kFIR_Type_HalfBand_15
+
+// 1/3 band filters (fs/6)
+#define kFIR_Type_ThirdBand_15	20
+#define kFIR_Type_ThirdBand_31	21
+#define kFIR_Type_ThirdBand	kFIR_Type_ThirdBand_15
+
+// Triangle filters 
+#define kFIR_Type_Triangle_3	30
+#define kFIR_Type_Triangle_9	31
 
 typedef struct fir {
 // High level data
@@ -49,47 +62,62 @@ void UpdateFIR (FIR *d);
 void ResetFIR  (FIR *d);
 void PrepareFIR(FIR *d);
 
-void RunFIR_Shorts(short *in, short *out, long length, FIR *d);
-void RunFIRFixedPoint_Shorts(short *in, short *out, long length, FIR *d);
+void RunFIR_Shortsf(short *in, short *out, long length, FIR *d);
+void RunFIR_Shortsi(short *in, short *out, long length, FIR *d);
 
-#define kFIR_HalfBandV1_Length 15
-extern float firHalfBand_V1_GainCompensationDB;
-extern float firHalfBandHz_V1[kFIR_HalfBandV1_Length];
 
-#define kFIR_HalfBand31_Length 31
-extern float firHalfBand_31_GainCompensationDB;
-extern float firHalfBand31Hz[kFIR_HalfBand31_Length];
+#define kFIR_Triangle_3_Hz_Length 3
+extern float fir_Triangle_3_GainCompensationDB;
+extern float fir_Triangle_3_Hz[kFIR_Triangle_3_Hz_Length];
 
-#define kFIR_BandPassHz_Length	15
-extern float firBandPass_GainCompensationDB;
-extern float firBandPassHz[kFIR_BandPassHz_Length];
+#define kFIR_Triangle_9_Hz_Length 9
+extern float fir_Triangle_9_GainCompensationDB;
+extern float fir_Triangle_9_Hz[kFIR_Triangle_9_Hz_Length];
 
-#define kFIR_BandStopHz_Length	17
-extern float firBandStop_GainCompensationDB;
-extern float firBandStopHz[kFIR_BandStopHz_Length];
+#define kFIR_BandPass_Hz_Length	15
+extern float fir_BandPass_GainCompensationDB;
+extern float fir_BandPassHz[kFIR_BandPass_Hz_Length];
+
+#define kFIR_BandStop_Hz_Length	17
+extern float fir_BandStop_GainCompensationDB;
+extern float fir_BandStopHz[kFIR_BandStop_Hz_Length];
 
 #define kFIR_Notch_Hz_Length 15
 extern float fir_Notch_GainCompensationDB;
 extern float fir_Notch_Hz[kFIR_Notch_Hz_Length];
 
-#define kFIR_HighPassHz_Length	15
-extern float firHighPass_GainCompensationDB;
-extern float firHighPassHz[kFIR_HighPassHz_Length];
+#define kFIR_HighPass_Hz_Length	15
+extern float fir_HighPass_GainCompensationDB;
+extern float fir_HighPass_Hz[kFIR_HighPass_Hz_Length];
 
-#define kFIR_LowPassHz_Length	15
-extern float firLowPass_GainCompensationDB;
-extern float firLowPassHz[kFIR_LowPassHz_Length];
+#define kFIR_LowPass_Hz_Length	15
+extern float fir_LowPass_GainCompensationDB;
+extern float fir_LowPass_Hz[kFIR_LowPass_Hz_Length];
 
-#define kFIR_LowPass_HalfBandHz_Length	15
-extern float firLowPass_HalfBand_GainCompensationDB;
-extern float firLowPass_HalfBandHz[kFIR_LowPass_HalfBandHz_Length];
+// Half band filters
+#define kFIR_HalfBand_32dB_Hz_Length	15
+extern float fir_HalfBand_32dB_GainCompensationDB;
+extern float fir_HalfBand_32dB_Hz[kFIR_HalfBand_32dB_Hz_Length];
 
-#define kFIR_LowPass58dB_HalfBandHz_Length	31
-extern float firLowPass58dB_HalfBand_GainCompensationDB;
-extern float firLowPass58dB_HalfBandHz[kFIR_LowPass58dB_HalfBandHz_Length];
+#define kFIR_HalfBand_58dB_Hz_Length	31
+extern float fir_HalfBand_58dB_GainCompensationDB;
+extern float fir_HalfBand_58dB_Hz[kFIR_HalfBand_58dB_Hz_Length];
 
-#define kFIR_ThirdBandV1_Hz_Length 15
-extern float fir_ThirdBandV1_GainCompensationDB ;
-extern float fir_ThirdBandV1_Hz[kFIR_ThirdBandV1_Hz_Length];
+#define kFIR_HalfBand_15_Hz_Length 15
+extern float fir_HalfBand_15_GainCompensationDB;
+extern float fir_HalfBand_15_Hz[kFIR_HalfBand_15_Hz_Length];
+
+#define kFIR_HalfBand_31_Hz_Length 31
+extern float fir_HalfBand_31_GainCompensationDB;
+extern float fir_HalfBand_31_Hz[kFIR_HalfBand_31_Hz_Length];
+
+// Third band filters
+#define kFIR_ThirdBand_15_Hz_Length 15
+extern float fir_ThirdBand_15_GainCompensationDB ;
+extern float fir_ThirdBand_15_Hz[kFIR_ThirdBand_15_Hz_Length];
+
+#define kFIR_ThirdBand_31_Hz_Length 31
+extern float fir_ThirdBand_31_GainCompensationDB;
+extern float fir_ThirdBand_31_Hz[kFIR_ThirdBand_31_Hz_Length]; 
 
 #endif  //	end __FIR_H__
