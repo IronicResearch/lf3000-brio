0667146b03dd853a95c81c8e5b33392665f96ff9
Dmilici <Dmilici@leapfrog.com>
2007-07-20 13:17:21 -0700

    Updated Tremor lib setup script for building from SVN source.
    (SVN Revision 950 from svn://localhost/TRUNK/)

--- ThirdParty/Theora/Source/setup-tremor.sh
+++ ThirdParty/Theora/Source/setup-tremor.sh
@@ -17,11 +17,11 @@ check_user
 # parse args
 set_standard_opts $*
 
-pushd $PROJECT_PATH/packages/tremor/
+pushd .
 
 if [ "$CLEAN" == "1" -o ! -e $TREMOR_LIB_DIR ]; then
 	rm -rf $TREMOR_LIB_DIR
-	svn checkout http://svn.xiph.org/trunk/Tremor/ $TREMOR_LIB_DIR
+	svn export http://svn.xiph.org/trunk/Tremor/ $TREMOR_LIB_DIR
 fi
 
 # build and copy shared libs to rootfs
@@ -29,8 +29,8 @@ pushd $TREMOR_LIB_DIR
 ./autogen.sh --host=arm-linux --build=x86-linux --prefix=$ROOTFS_PATH/usr/local --enable-shared=yes LDFLAGS="-L$ROOTFS_PATH/usr/local/lib -Wl,--rpath-link -Wl,$ROOTFS_PATH/usr/local/lib" 
 make
 # make install
-cp -a ./.libs/libvorbisidec.so* $ROOTFS_PATH/usr/local/lib/
-cp    ./*.h $ROOTFS_PATH/usr/local/include/tremor/
+# cp -a ./.libs/libvorbisidec.so* $ROOTFS_PATH/usr/local/lib/
+# cp    ./*.h $ROOTFS_PATH/usr/local/include/tremor/
 popd
 
 popd
