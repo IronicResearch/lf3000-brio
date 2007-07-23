d3bbe4feb04f0510d46b00fbc6f59ad6e56e469e
TParker <TParker@leapfrog.com>
2007-07-23 14:56:37 -0700

    Pre sort packed files (case insensitive); avoid packing duplicate files; errors for multiply defined URIs; add new resource types.
    (SVN Revision 964 from svn://localhost/TRUNK/)

--- Etc/Tools/SConsTools/LightningUtils.py
+++ Etc/Tools/SConsTools/LightningUtils.py
@@ -13,7 +13,12 @@ import glob
 import SCons.Options
 import SCons.Script
 import SCons.Util
+import SCons.Defaults
 	
+
+this_dir		= os.path.split(__file__)[0]
+cdevkit_dir		= os.path.normpath(os.path.join(this_dir, '..'))
+toolpath		= os.path.join(cdevkit_dir, 'Tools')
 
 #-----------------------------------------------------------------------------
 # Setup common command line options for a Lightning project
@@ -75,18 +80,15 @@ def RetrieveOptions(args, root_dir):
 def CreateEnvironment(opts, vars):
 
 	target_subdir	= vars['target_subdir']
-	this_dir		= os.path.split(__file__)[0]
-	cdevkit_dir		= os.path.normpath(os.path.join(this_dir, '..'))
-	toolpath		= os.path.join(cdevkit_dir, 'Tools')
 	
 	platform_toolset = vars['platform'] + (vars['is_emulation'] and '_emulation' or '_embedded')
 
 	env = SCons.Environment.Environment(options  = opts,
-							tools = ['default', platform_toolset, 'cxxtest', 'runtest', 'checkheader'], 
+							tools = ['default', platform_toolset, 'cxxtest', 'runtest', 'checkheader', 'oggenc'], 
 							toolpath = [toolpath],
 					 )
-	env.Append(CPPDEFINES = ['LF2530BLUE'])
-	#FIXME/tp: Keep this 'variant' field up to date!
+	env.Append(CPPDEFINES = ['LF2530BROWN'])
+	#FIXME/tp: Keep this 'variant' field up to date! Remove at final hardware.
 	
 	cdevkit_incpath	= os.path.join(cdevkit_dir, 'Include')
 	ogl_incpath		= os.path.join(cdevkit_incpath, 'OpenGL', target_subdir)
@@ -161,7 +163,7 @@ def MakeMyApp(penv, ptarget, psources, plibs, vars):
 		platformlibs = ['gles_cl', 'glibmm-2.4', 'glib-2.0', 'Emulation']
 	else:
 		platformlibs = ['ogl', 'dl', 'pthread', 'ustring', 'iconv', 'intl', 'sigc-2.0']
-		#FIXME/tp: pthread should go away (curr needed by BrioCube?)
+		#FIXME/tp: pthread should go away
 		CopyResources(penv, vars)
 	
 	# Set up targets
@@ -181,10 +183,27 @@ def MakeMyApp(penv, ptarget, psources, plibs, vars):
 	
 	return objs
 
+
+
+#=========================================================================
+# *** RESOURCE PACKING ***
+#=========================================================================
+#-------------------------------------------------------------------------
+# Hooks to encoders
+#-------------------------------------------------------------------------
+sEnv		= SCons.Environment.Environment(tools = ['oggenc', 'rawenc'], 
+							toolpath = [toolpath])
+
+#-------------------------------------------------------------------------
+# Cache state to avoid duplicate packing and report multiple definitions
+#-------------------------------------------------------------------------
+sSourceToDestMap = {}
+sResourceURIMap  = {}
+
 #-------------------------------------------------------------------------
 # Functions to enumerate packed filenames
 #-------------------------------------------------------------------------
-valid_chars = range(48,58) + range (65,91)
+valid_chars		= range(48,58) + range (65,91)
 valid_chars_count = len(valid_chars)
 sPkgNameIndex	= [0, -1]
 sRsrcNameIndex	= [0, -1]
@@ -236,19 +255,69 @@ def MapAcmeActivitiesToURIs(data_root):
 # Set up a map of type names and file extensions to numeric type values
 #-------------------------------------------------------------------------
 def SetupTypeConversionMap():
-	types = { 'mid' 	: 1025,
-			  'midi' 	: 1025,
-			  'S'		: 1025,				# for ACME
-			  'aogg'	: 1026,
-			  'ogg'		: 1024 * 14 + 1,
-			  'wav'		: 1027,
-			  'R'		: 1027,				# for ACME
-			  'raw'		: 1027,				# for ACME
-			  'font'	: 1024 * 7 + 1,
-			  'ttf'		: 1024 * 7 + 1,
-			  'txt'		: 1024 * 4 + 1,
+	types = { 'mid' 	: 1024 *  1 + 1,	#Audio Group
+			  'midi' 	: 1024 *  1 + 1,
+			  'S'		: 1024 *  1 + 1,		# for ACME
+			  'aogg'	: 1024 *  1 + 2,
+			  'wav'		: 1024 *  1 + 3,
+			  'R'		: 1024 *  1 + 3,		# for ACME
+			  'raw'		: 1024 *  1 + 3,		# for ACME
+			  'txt'		: 1024 *  4 + 1,	# Common Group
+			  'bin'		: 1024 *  4 + 2,
+			  'json'	: 1024 *  4 + 3,
+			  'xml'		: 1024 *  4 + 4,
+			  'bmp'		: 1024 *  5 + 1,	# Display Group
+			  'gif'		: 1024 *  5 + 2,
+			  'jpg'		: 1024 *  5 + 3,
+			  'jpeg'	: 1024 *  5 + 3,
+			  'png'		: 1024 *  5 + 4,
+			  'font'	: 1024 *  7 + 1,	# Font Group
+			  'ttf'		: 1024 *  7 + 1,
+			  'ogg'		: 1024 * 14 + 1,	# Video Group
 			  }
 	return types
+		
+#-------------------------------------------------------------------------
+# Track duplicate URIs
+#-------------------------------------------------------------------------
+def AddURILocation(uri, file, line):
+	key = uri.upper()
+	locations = []
+	if key in sResourceURIMap:
+		locations = sResourceURIMap[key]
+	locations += [[file, line]]
+	sResourceURIMap[key] = locations
+
+#-------------------------------------------------------------------------
+# Transform or copy source file to packed output file
+#-------------------------------------------------------------------------
+def PackFile(srcfile, pack_root, type, compression, rate):
+	# 1) If we have already processed this source file, return the
+	#    already processed destination file
+	# 2) Create output file path
+	# 3) Add the new output file to the Source->Dest map, return it
+	# 4) Invoke OGG encoder
+	# 5) Invoke utility that converts WAV to RAW PCM w/Brio header
+	# A) No encoder needed, just copy the file
+	#
+	if sSourceToDestMap.has_key(srcfile):							#1
+		return sEnv.File(sSourceToDestMap[srcfile])
+		
+	outname = GenerateNextResourceFileName()						#2
+	outfile = os.path.join(pack_root, outname)
+	sSourceToDestMap[srcfile] = outfile								#3
+	
+	if type == 1026 and rate != '':									#3
+		enc = sEnv.OggEnc(outfile, srcfile, OGGENC_RATE=rate, OGGENC_COMPRESSION=compression)
+		
+	elif type == 1027:												#5
+		enc = sEnv.RawEnc(outfile, srcfile)
+		
+	else:
+		enc = sEnv.Command(outfile, srcfile, [SCons.Defaults.Copy('$TARGET', '$SOURCE')])	#A
+		
+	return enc[0]
+
 
 #-------------------------------------------------------------------------
 # Pack contents of an individual ACME CSV file
@@ -263,75 +332,88 @@ def ProcessAcme(pkg, types, pack_root, data_root, enumpkg):
 	#   1: "handle" or URL node value
 	#   2: data type string 
 	#   3: file name
-	#
+	#	
 	fld = [7, 17, 15, 9]	# activity, handle, type, file			#1
 	dict = MapAcmeActivitiesToURIs(data_root)						#2
+	resources = []
 	
 	reader = csv.reader(open(pkg, "rb"))
 	pkguri = dict[os.path.basename(pkg)]
 	pkgfile = GenerateNextPackageFileName()
-	enumpkg.writerow([pkguri, pkgfile])
-	writer = csv.writer(open(os.path.join(pack_root, pkgfile), "w"))
-	linenum = 0
+	packages += [[pkguri, pkgfile, 1, 1]]
+	line = 0
 	for row in reader:
-		if linenum == 0:
+		line += 1
+		if line == 0:
 			# TODO: map 'fld' to 'SHAPE DESCRIPTION', 'FILE', 'AUDIOTYPE', & 'AUDIO HANDLE'
-			linenum += 1
+			dummy = line	
 		elif row[fld[3]] != '':
+			compression = rate = ''
 			if types.has_key(row[fld[2]]):
 				type = types[row[fld[2]]]
 			else:
 				type = 1025
 			extension = type == 1024 and '.mid' or '.wav'
-			outfile = GenerateNextResourceFileName()
 			srcfile = os.path.join(data_root, row[fld[3]]+extension)
 			srcsize = os.path.getsize(srcfile)
-			writer.writerow([dict[row[fld[0]]] + row[fld[1]], type, outfile, srcsize, srcsize, 1])
-			shutil.copyfile(srcfile, os.path.join(pack_root, outfile))
+			outfile = PackFile(srcfile, pack_root, type, compression, rate)
+			outsize = outfile.getsize()
+			uri = dict[row[fld[0]]] + row[fld[1]]
+			AddURILocation(uri, pkg, line)
+			resources += [[uri, type, outfile.abspath, outsize, srcsize, version]]
+
+	deco = [ (res[0].upper(), i, res) for i, res in enumerate(resources) ]
+	deco.sort()														#8
+	resources = [ res for _, _, res in deco ]
+	writer = csv.writer(open(os.path.join(pack_root, pkgfile), "w"))
+	writer.writerows(resources)
+
 
 #-------------------------------------------------------------------------
 # Pack contents of an individual package
 #-------------------------------------------------------------------------
-def ProcessPackage(pkg, types, pack_root, data_root, enumpkg):
+def ProcessPackage(pkg, types, pack_root, data_root, packages):
 	# Package input format is "baseURIPath, URINode, srcFile, (optional)packtype"
 	#
 	# 1) Open input and output packages
 	# 2) Skip comment lines
 	# 3) The first non-comment line of the input package contains default
 	#    values so parse and store them.  This info can be used to build
-	#    the package URI, so
-	#    add the URI -> path mapping for the package to the EnumPkgs file
+	#    the package URI, so add the URI -> path mapping for the package
+	#    to the EnumPkgs file.
 	# 4) Subsequent lines contain resources
-	# 5) Get the type info either from the type field or the file extension
+	# 5) Get the type info either from the type field or the file extension.
+	#    Field 4 of the CSV file is a combined type/compression/rate field, 
+	#    joined together by underscores.  An example would be "AOGG_3_16000"
+	#    for and audio OGG file compressed at quality 3 at a sample rate of
+	#    16000.  Parse that into its constituent parts.
 	# 6) Get the base URL from the line and use the default if not present
-	# 7) Output data describing the resource in the package file
-	# 8) Either copy or transform & copy the file from the source
-	#    folder to the destination folder
-	# NOTE: The sf2brio EXE is a temporary "packer" that puts a header
-	#		in front of a WAV file.  It will be replaced by the OggVorbis
-	#		encoder.
-	# Field 4 of the CSV file is a combined type/compression/rate field, 
-	# joined together by underscores.  An example would be "AOGG_3_16000"
-	# for and audio OGG file compressed at quality 3 at a sample rate of
-	# 16000.
+	# 7) Get the source file name and pack or copy the source to the output.
+	#    Add a line describing the resource to the "resources" list.
+	# 8) Sort the resources list by URI (case-insenstive) and write out
+	#    the output package file (see the following URL for an explanation
+	#    of this sorting method: http://wiki.python.org/moin/HowTo/Sorting)
 	#
 	reader = csv.reader(open(pkg, "rb"))							#1
 	pkgfile = GenerateNextPackageFileName()
-	writer = csv.writer(open(os.path.join(pack_root, pkgfile), "w"))
-	linenum = 0
+	line = 0
 	defaultBase = ''
 	defaultVersion = '1'
+	isDefaultRow = True
+	resources = []
 	
 	for row in reader:
+		line += 1
 		if len(row) == 0 or (row[0] and row[0][0] == '#'):			#2
 			continue
-		if linenum == 0:											#3
+			
+		if isDefaultRow:											#3
 			defaultBase = row[0].strip()
 			if len(row) >= 5:
 				defaultVersion = row[4].strip()
 			pkguri = defaultBase + os.path.splitext(os.path.basename(pkg))[0]
-			enumpkg.writerow([pkguri, pkgfile, 1, 1])
-			linenum += 1
+			packages += [[pkguri, pkgfile, 1, 1]]
+			isDefaultRow = False
 			continue												#4
 			
 		compression = rate = ''
@@ -350,36 +432,39 @@ def ProcessPackage(pkg, types, pack_root, data_root, enumpkg):
 		base = row[0].strip() and row[0].strip() or defaultBase		#6
 		version = len(row) >= 5 and row[5].strip() and row[5].strip() or defaultVersion
 
-		outfile = GenerateNextResourceFileName()					#7
-		outpath = os.path.join(pack_root, outfile)
-		srcfile = os.path.join(data_root, row[2].strip())
+		srcfile = os.path.join(data_root, row[2].strip())			#7
 		srcsize = os.path.getsize(srcfile)
-		#FIXME/tp: store in list, sort, then write whole list to file
-		writer.writerow([base + row[1].strip(), type, outfile, srcsize, srcsize, version])
-		
-		this_dir	= os.path.split(__file__)[0]
-		oggenc		= 'LD_LIBRARY_PATH=' + this_dir + ' ' + os.path.join(this_dir, 'oggenc')	
-		if type == 1026 and rate != '':								#8
-			cmd = oggenc +' --resample ' + rate + ' -q ' + compression + ' -o ' + outpath + ' ' + srcfile
-			os.system(cmd)
-		elif type == 1027:											#9
-			os.system(os.path.join(this_dir, 'sf2brio') + ' ' + srcfile + ' ' + outpath)
-		else:
-			shutil.copyfile(srcfile, outpath)
+		outfile = PackFile(srcfile, pack_root, type, compression, rate)
+		outsize = outfile.getsize()
+		uri = base + row[1].strip()
+		AddURILocation(uri, pkg, line)
+		resources += [[uri, type, outfile.abspath, outsize, srcsize, version]]
+
+	deco = [ (res[0].upper(), i, res) for i, res in enumerate(resources) ]
+	deco.sort()														#8
+	resources = [ res for _, _, res in deco ]
+	writer = csv.writer(open(os.path.join(pack_root, pkgfile), "w"))
+	writer.writerows(resources)
 
 
 #-------------------------------------------------------------------------
 # Deploy the apprsrc assets for embedded builds
 #-------------------------------------------------------------------------
 def PackAllResources(penv):
+	# 0) Do nothing for "clean" operations
 	# 1) Setup source and destination folders
-	# 2) Open the rsrc/EnumPkgs file for writing
-	# 3) Find all "ACME" input package defintion files and process them
-	# 4) Find all standard packag input files and process them
+	# 2) Find all "ACME" input package definition files and process them
+	# 3) Find all standard package input files and process them
+	# 4) Sort the packages and report any duplicate package URIs
+	# 5) Report any duplicate package names
+	# 6) Write them to the rsrc/EnumPkgs file
 	#
-	if penv.GetOption('clean'):
+	if penv.GetOption('clean'):										#0
 		return
-		
+	
+	sSourceToDestMap = {}
+	sResourceURISet = {}
+	packages = []
 	data_root = penv.Dir('#apprsrc').abspath						#1
 	build_root = penv.Dir('#Build').abspath
 	pack_root = penv.Dir('#Build/rsrc').abspath
@@ -388,22 +473,48 @@ def PackAllResources(penv):
 	if not os.path.exists(pack_root):
 		os.mkdir(pack_root)
 	types = SetupTypeConversionMap()
+	penv.Default(pack_root)
 	
-	enumpkg_path = os.path.join(pack_root, "EnumPkgs")				#2
-	enumpkg = csv.writer(file(enumpkg_path, "w"))
+	pkgs = glob.glob(os.path.join(data_root, '*.acme'))				#2
+	for pkg in pkgs:
+		ProcessAcme(pkg, types, pack_root, data_root, packages)
 		
-	packages =  glob.glob(os.path.join(data_root, '*.acme'))		#3
-	for pkg in packages:
-		ProcessAcme(pkg, types, pack_root, data_root, enumpkg)
-		
-	packages =  glob.glob(os.path.join(data_root, '*.pkg'))			#4
-	for pkg in packages:
-		ProcessPackage(pkg, types, pack_root, data_root, enumpkg)
+	pkgs = glob.glob(os.path.join(data_root, '*.pkg'))				#3
+	for pkg in pkgs:
+		ProcessPackage(pkg, types, pack_root, data_root, packages)
 	
+	enumpkg_path = os.path.join(pack_root, "EnumPkgs")				#4
+	deco = [ (pkg[0].upper(), i, pkg) for i, pkg in enumerate(packages) ]
+	deco.sort()
+	n = len(deco)
+	last, _, _ = deco[0]
+	i = 1
+	while i < n:
+		next, _, _ = deco[i]
+		if next == last:
+			_, _, duplicate = deco[i]
+			print 'ERROR: Duplicate package URIs used:', duplicate[0]
+			raise 'ERROR: Duplicate package URIs used!'
+		last = next
+		i += 1
 
-#TODO: Deal with greater than 36^2 resources (hierarchical folders)
-#TODO: Improve error handling and reporting!
-#TODO: Make packing a 'Tool' so it only runs when sources are touched
+	sawError = False												#5
+	for k, v in sResourceURIMap.iteritems():
+		if len(v) > 1:
+			print 'ERROR: Duplicate URIs found on the following lines:'
+			for vals in v:
+				print '\t', vals[0], 'line', vals[1]
+			sawError = True
+	if sawError:
+		raise 'Duplicate URIs found, aborting!'
+	
+	packages = [ pkg for _, _, pkg in deco ]						#6
+	writer = csv.writer(file(enumpkg_path, "w"))
+	writer.writerows(packages)
+
+#TODO: Deal with greater than 36^2 resources (hierarchical folders).  Currently an exception is raised.
+#TODO: Make packing a 'Tool' so it only runs when sources are touched (a biggie!)
+#TODO: Define method for app-specific resource type values that don't overlap
 
 #-----------------------------------------------------------------------------
 # Export the all of the functions symbols
