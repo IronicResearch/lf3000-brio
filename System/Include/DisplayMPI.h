eb1c472e396389560ca7d6a4c8ce34a5a7ccd0b2
Dmilici <Dmilici@leapfrog.com>
2007-07-25 13:14:23 -0700

    Annotated with comments summarizing Display MPI interface methods.
    (SVN Revision 1016 from svn://localhost/TRUNK/)

--- System/Include/DisplayMPI.h
+++ System/Include/DisplayMPI.h
@@ -34,42 +34,93 @@ public:
 	virtual ~CDisplayMPI();
 
 	// Screen functionality
+	//=====================
+	
+	// Returns number of display output screens
 	U16							GetNumberOfScreens() const;
+	
+	// Returns screen info for the selected output device (width, height, pixel format, description)
 	const tDisplayScreenStats*	GetScreenStats(tDisplayScreen screen) const;
+	
+	// Updates the display output, either selected rectangular region or entire screen
 	tErrType					Invalidate(tDisplayScreen screen, tRect *pDirtyRect = NULL);
 
 	// Graphics contexts (through tDisplayHandle)
+	//===========================================
+	
+	// Creates a display surface context for access by the application
 	tDisplayHandle      CreateHandle(U16 height, U16 width, tPixelFormat colorDepth, 
 									U8 *pBuffer = NULL);
+
+	// Returns the buffer address in user memory space of the display surface
 	U8*                 GetBuffer(tDisplayHandle hndl) const;
+
+	// Returns the bytes per scanline pitch (stride) of the display surface
 	U16                 GetPitch(tDisplayHandle hndl) const;
+	
+	// Returns the height of the display surface
 	U16                 GetHeight(tDisplayHandle hndl) const;
+	
+	// Returns the width of the display surface
 	U16                 GetWidth(tDisplayHandle hndl) const;
 	
+	// Positions the display surface on the screen	
 	tErrType            Register(tDisplayHandle hndl, S16 xPos = 0, S16 yPos = 0, 
 								 tDisplayHandle insertAfter = 0, 
 								 tDisplayScreen screen = kDisplayScreenAllScreens);
+
+	// Positions the display surface on the screen	
 	tErrType            Register(tDisplayHandle hndl, S16 xPos = 0, S16 yPos = 0, 
 								 tDisplayZOrder initialZOrder = kDisplayOnTop, 
 	                             tDisplayScreen screen = kDisplayScreenAllScreens);
+
+	// Removes the display surface from the screen
 	tErrType            UnRegister(tDisplayHandle hndl, tDisplayScreen screen);
 	
+	// Destroys the display surface context created by CreateHandle()
 	tErrType            DestroyHandle(tDisplayHandle hndl, Boolean destroyBuffer);
 	
+	// Locks the display surface framebuffer memory for exclusive access by the application
 	tErrType            LockBuffer(tDisplayHandle hndl);
+	
+	// Unlocks the display surface framebuffer memory locked by LockBuffer()
 	tErrType            UnlockBuffer(tDisplayHandle hndl, tRect *pDirtyRect = NULL);
+	
+	// Sets the alpha transparency value for the display surface layer
 	tErrType			SetAlpha(tDisplayHandle hndl, U8 level, 
 								Boolean enable=true);
-	
+
+	// OpenGL context interface
+	//=========================
+
+	// Initializes the display manager for OpenGL rendering contexts
 	void				InitOpenGL(void* pCtx);
+	
+	// Shutdown display manager OpenGL context setup by InitIOpenGL()
 	void				DeinitOpenGL();
+	
+	// Enables OpenGL hardware 3D engine rendering output to display surface layer
 	void				EnableOpenGL();
+	
+	// Disables OpenGL hardware 3D engine rendering output to display surface layer
 	void				DisableOpenGL();
+
+	// Updates OpenGL hardware 3D engine rendering output to display surface layer
 	void				UpdateOpenGL();
 	
+	// Brightness and contrast controls
+	//=================================
+
+	// Sets the brightness of the display output screen (0 = default setting)
 	tErrType			SetBrightness(tDisplayScreen screen, S8 brightness);
+	
+	// Sets the contrast of the display output screen (0 = default setting)
 	tErrType			SetContrast(tDisplayScreen screen, S8 contrast);
+	
+	// Gets the brightness setting of the display output screen
 	S8					GetBrightness(tDisplayScreen screen);
+	
+	// Gets the contrast setting of the display output screen
 	S8					GetContrast(tDisplayScreen screen);
 	
 private:
