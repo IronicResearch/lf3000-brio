585da348cb6cfefb88028a69cc6bfea1529b5896
Dave Milici <dmilici@leapfrog.com>
2007-09-10 14:41:01 -0700

    Added pixel format conversion to non-default X pixmap formats.
    (SVN Revision 1534 from svn://localhost/TRUNK/)

--- System/Display/Emulation/DisplayEmul.cpp
+++ System/Display/Emulation/DisplayEmul.cpp
@@ -158,19 +158,23 @@ tDisplayHandle CDisplayModule::CreateHandle(U16 height, U16 width,
 {
 	tDisplayContext *dc =  new struct tDisplayContext;
 
-	int depth;
+	// X window needs pixmap of same color depth as desktop ("24")
+	int depth,bpp;
 	switch (colorDepth) 
 	{
-		case kPixelFormatRGB4444:	depth = 16; break;
-		case kPixelFormatRGB565:	depth = 16; break;
-		case kPixelFormatARGB8888: 	
-		default:					depth = 24; //32; break;
+		case kPixelFormatRGB4444:	bpp = 16; depth = 24; break;
+		case kPixelFormatRGB565:	bpp = 16; depth = 24; break;
+		case kPixelFormatRGB888:	bpp = 24; depth = 24; break; 	
+		default:
+		case kPixelFormatARGB8888: 	bpp = 32; depth = 24; break;
+		case kPixelFormatYUV420:	bpp = 8;  depth = 24; break;
+		case kPixelFormatYUYV422:	bpp = 16; depth = 24; break;
 	}		
 
 	dc->width = width;
 	dc->height = height;
 	dc->colorDepthFormat = colorDepth;
-
+	
 	// YUV planar format needs double-height surface for U and V buffers
 	if (colorDepth == kPixelFormatYUV420)
 		height *= 2;
@@ -181,8 +185,8 @@ tDisplayHandle CDisplayModule::CreateHandle(U16 height, U16 width,
 	
 	dc->pixmap = pixmap;
 	dc->image = image;
-	dc->depth = image->bitmap_unit;
-	dc->bpp = image->bits_per_pixel;
+	dc->depth = bpp; // image->bitmap_unit;
+	dc->bpp = bpp/8; // image->bits_per_pixel;
 	dc->pitch = image->bytes_per_line;
 	dc->pBuffer = (U8*)image->data;
 	dc->x = 0;
@@ -216,6 +220,75 @@ tErrType CDisplayModule::UnRegisterLayer(tDisplayHandle hndl)
 {
 	// Nothing to do on emulation target
 }
+
+//----------------------------------------------------------------------------
+void RGB4444ARGB(tDisplayContext* dc)
+{
+	// Repack 16bpp RGB4444 format surface into 32bpp ARGB format surface
+	U8			buffer[WINDOW_WIDTH*2];
+	U8* 		s = &buffer[0];
+	U8*			d = reinterpret_cast<U8*>(dc->image->data);
+	int			i,j,m,n;
+	for (i = 0; i < dc->height; i++) 
+	{
+		memcpy(s, d, dc->width*2);
+		for (j = m = n = 0; j < dc->width; j++, m+=2, n+=4) 
+		{
+			U16 color = (s[m+1] << 8) | (s[m]);
+			d[n+0] = ((color & 0x000F) >> 0) << 4;
+			d[n+1] = ((color & 0x00F0) >> 4) << 4;
+			d[n+2] = ((color & 0x0F00) >> 8) << 4;
+			d[n+3] = ((color & 0xF000) >> 12) << 4;
+		}
+		d += dc->pitch;
+	}
+}
+
+//----------------------------------------------------------------------------
+void RGB565ARGB(tDisplayContext* dc)
+{
+	// Repack 16bpp RGB565 format surface into 32bpp ARGB format surface
+	U8			buffer[WINDOW_WIDTH*2];
+	U8* 		s = &buffer[0];
+	U8*			d = reinterpret_cast<U8*>(dc->image->data);
+	int			i,j,m,n;
+	for (i = 0; i < dc->height; i++) 
+	{
+		memcpy(s, d, dc->width*2);
+		for (j = m = n = 0; j < dc->width; j++, m+=2, n+=4) 
+		{
+			U16 color = (s[m+1] << 8) | (s[m]);
+			d[n+0] = ((color & 0x001F) >> 0) << 3;
+			d[n+1] = ((color & 0x07E0) >> 5) << 2;
+			d[n+2] = ((color & 0xF800) >> 11) << 3;
+			d[n+3] = 0xFF;
+		}
+		d += dc->pitch;
+	}
+}
+
+//----------------------------------------------------------------------------
+void RGB2ARGB(tDisplayContext* dc)
+{
+	// Repack 24bpp RGB888 format surface into 32bpp ARGB format surface
+	U8			buffer[WINDOW_WIDTH*3];
+	U8* 		s = &buffer[0];
+	U8*			d = reinterpret_cast<U8*>(dc->image->data);
+	int			i,j,m,n;
+	for (i = 0; i < dc->height; i++) 
+	{
+		memcpy(s, d, dc->width*3);
+		for (j = m = n = 0; j < dc->width; j++, m+=3, n+=4) 
+		{
+			d[n+0] = s[m+0];
+			d[n+1] = s[m+1];
+			d[n+2] = s[m+2];
+			d[n+3] = 0xFF;
+		}
+		d += dc->pitch;
+	}
+}
+
 //----------------------------------------------------------------------------
 // YUV to RGB color conversion: <http://en.wikipedia.org/wiki/YUV>
 inline 	U8 clip(S16 X)			{ return (X < 0) ? 0 : (X > 255) ? 255 : static_cast<U8>(X); }
@@ -305,6 +378,12 @@ tErrType CDisplayModule::Update(tDisplayContext* dc)
 		YUV2ARGB(dc);
 	else if (dc->isOverlay)
 		YUYV2ARGB(dc);
+	else if (dc->colorDepthFormat == kPixelFormatRGB888)
+		RGB2ARGB(dc);
+	else if (dc->colorDepthFormat == kPixelFormatRGB565)
+		RGB565ARGB(dc);
+	else if (dc->colorDepthFormat == kPixelFormatRGB4444)
+		RGB4444ARGB(dc);
 		
 	XPutImage(x11Display, x11Window, gc, dc->image, 0, 0, dc->rect.left, dc->rect.top, dc->rect.right - dc->rect.left, dc->rect.bottom - dc->rect.top);
 	XFlush(x11Display);
