12a6653c76964e56e8ba9329525df29710b6119d
gklimanis <gklimanis@leapfrog.com>
2007-10-12 13:24:26 -0700

    Shells, soundfiles, etc updates to Ogg performance measurement program.
    (SVN Revision 1910 from svn://localhost/TRUNK/)

--- System/Audio/Utilities/sf2brio/go.sh
+++ System/Audio/Utilities/sf2brio/go.sh
@@ -1,1 +1,5 @@
-./sfconvert
+./sf2brio belltree_up2.wav out.brio
+./sf2brio -outFormat wav belltree_up2.wav out.wav
+./sf2brio -outFormat raw belltree_up2.wav out.raw
+./sf2brio -outFormat raw belltree_up2.wav out2.brio
+
