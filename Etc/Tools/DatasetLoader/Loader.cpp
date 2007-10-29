ccc5ff42881cec943347d78e56cd8f9ee1d07618
Rcabrera <Rcabrera@leapfrog.com>
2007-10-29 13:15:37 -0700

    Check-in DatasetLoader source codes.
    (SVN Revision 2080 from svn://localhost/TRUNK/)

---
+++ Etc/Tools/DatasetLoader/Loader.cpp
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <vector>	
+#include <string>	
+#include <fstream> 
+#include <fcntl.h>
+#include <math.h>
+#include <SystemTypes.h>
+#include <CoreTypes.h>	
+#include <boost/shared_array.hpp>	
+
+#include "Wrappers.h"
+
+#ifndef O_BINARY
+#define O_BINARY 0
+#endif 
+
+using namespace std;
+LF_USING_BRIO_NAMESPACE()
+
+const string kDirSeparatorString("\\");	
+
+typedef vector<U32>  PointerOffsets;
+
+typedef struct tagAppRsrcDataSet {
+		tVersion	fmtVersion;	
+		tVersion 	cntVersion;	
+		U32 		id;
+		void* 	pDataSet;
+}tAppRsrcDataSet;
+
+/****************************************************************************
+ * 																			*
+ * Dataset loader functions.												*
+ * 		StripTrailing() -remove whitespace when their second parameter      *
+ * 						 is defaulted, but can also be used to remove 		*
+ * 						 leading and trailing quote characters, etc.		*
+ * 		FileSize(a)		-returns size of a.                         		*
+ * 		OffsetsToPtrs()	-convert the offsets into pointers for a given   	*
+ * 						 pointer address.									*
+ * 		LoadDataset()	-loads the dataset into RAM, fixes up any pointers	*
+ * 						 and returns a pointer to the dataset.	            *
+ * **************************************************************************/
+//----------------------------------------------------------------------------
+string StripTrailing( const string& in, 
+						const string& remove /* = std_string(_T(" \t\r\n")) */ )
+{
+	string out(in);
+	while( out.size() && out.size() == out.find_last_of(remove) + 1 )
+		out = out.substr(0, out.size() - 1);
+	return out;
+}
+
+//----------------------------------------------------------------------------
+// FileSize()
+//----------------------------------------------------------------------------
+size_t FileSize( const string& file )
+{
+	struct stat status;
+	if( 0 == stat(StripTrailing(file, kDirSeparatorString).c_str(), &status) )
+		return status.st_size;
+	return 0;
+}
+
+//----------------------------------------------------------------------------
+void OffsetsToPtrs( U8* pData, const PointerOffsets& ptr_offsets )
+{
+	U8* pBase = pData;
+	for( PointerOffsets::const_iterator it = ptr_offsets.begin();
+			it != ptr_offsets.end(); ++it )
+	{
+		*((U32*)(pBase + *it)) += (U32)pBase;
+	}
+}
+
+tAppRsrcDataSet* LoadDataset(const string binPath, const string relinkPath)
+{
+	boost::shared_array<U8> buf(new U8[FileSize(binPath)]);
+					EFdWrapper fd(binPath, O_RDONLY | O_BINARY);
+					read(fd, buf.get(), FileSize(binPath));
+		
+	ifstream in("relink.txt");
+	U32 p;
+	vector<U32> pointers;
+	while (in >> p)
+	{
+		pointers.push_back(p);
+	}
+	
+	OffsetsToPtrs(buf.get(), pointers);
+	tAppRsrcDataSet* pMD = (tAppRsrcDataSet*)buf.get();
+			
+	return pMD;
+}
