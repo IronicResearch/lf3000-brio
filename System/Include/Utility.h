330b0062b6c0a20cce9e586921ac96eb01a713cc
Dave Milici <dmilici@leapfrog.com>
2007-10-30 15:13:22 -0700

    Added Utility library for Dataset Loader function.
    (SVN Revision 2102 from svn://localhost/TRUNK/)

---
+++ System/Include/Utility.h
@@ -0,0 +1,76 @@
+#ifndef LF_BRIO_UTILITY_H
+#define LF_BRIO_UTILITY_H
+//============================================================================
+// $Source: $
+//
+// Copyright (c) LeapFrog Enterprises, Inc.
+//============================================================================
+//
+// File:
+//		Utility.h
+//
+// Description:
+//		Utility functions.
+//
+//============================================================================
+#include <stdio.h>
+#include <vector>	
+#include <string>	
+#include <fstream> 
+#include <fcntl.h>
+#include <math.h>
+#include <SystemTypes.h>
+#include <CoreTypes.h>	
+#include <boost/shared_array.hpp>	
+
+#include "Wrappers.h"
+
+#ifndef O_BINARY
+#define O_BINARY 0
+#endif 
+
+using namespace std;
+LF_BEGIN_BRIO_NAMESPACE()
+
+//============================================================================
+// Constants
+//============================================================================
+const string kDirSeparatorString("\\");	
+
+typedef vector<U32>  PointerOffsets;
+
+typedef struct tagAppRsrcDataSet {
+		tVersion	fmtVersion;	
+		tVersion 	cntVersion;	
+		U32 		id;
+		void* 	pDataSet;
+}tAppRsrcDataSet;
+
+/****************************************************************************
+ * 																			*
+ * Dataset loader functions.												*
+ * 		StripTrailing() -remove whitespace when their second parameter      *
+ * 						 is defaulted, but can also be used to remove 		*
+ * 						 leading and trailing quote characters, etc.		*
+ * 		FileSize(a)		-returns size of a.                         		*
+ * 		OffsetsToPtrs()	-convert the offsets into pointers for a given   	*
+ * 						 pointer address.									*
+ * 		LoadDataset()	-loads the dataset into RAM, fixes up any pointers	*
+ * 						 and returns a pointer to the dataset.	            *
+ * **************************************************************************/
+//----------------------------------------------------------------------------
+string StripTrailing( const string& in, const string& remove);
+
+//----------------------------------------------------------------------------
+size_t FileSize( const string& file );
+
+//----------------------------------------------------------------------------
+void OffsetsToPtrs( U8* pData, const PointerOffsets& ptr_offsets );
+
+//----------------------------------------------------------------------------
+tAppRsrcDataSet* LoadDataset(const string binPath, const string relinkPath);
+
+LF_END_BRIO_NAMESPACE()
+
+#endif // LF_BRIO_UTILITY_H
+// EOF
---
+++ System/Include/Wrappers.h
@@ -0,0 +1,91 @@
+#ifndef LF_BRIO_WRAPPERS_H
+#define LF_BRIO_WRAPPERS_H
+
+#include <stdio.h>
+#include <string>	
+#include <fstream> 
+#include <fcntl.h>
+
+using namespace std;
+LF_USING_BRIO_NAMESPACE()
+
+//============================================================================
+// EFdWrapper class
+//============================================================================
+class EFdWrapper
+{
+	// Wrap a file descriptor so a file is closed in the dtor.
+	// Also provide a ctor that opens the file.
+	// Useful when file I/O errors may cause many different exit points
+	// from a routine that has an open file descriptor.
+public:
+	EFdWrapper( int fd = -1 );
+	EFdWrapper( const string& strFile, int oflag, int pmode = S_IWRITE );
+	~EFdWrapper( );
+	
+	bool Open( const string& strFile, int oflag, int pmode = S_IWRITE );
+	void Close( );
+	void Assign( int fd )	{ Close (); mfd = fd; }
+	bool IsValid( )			{ return (mfd != -1); }
+	operator int( )			{ return mfd; }
+	
+private:
+	EFdWrapper( const EFdWrapper& );
+	EFdWrapper& operator=( const EFdWrapper& );
+	
+	int		mfd;
+};
+
+//----------------------------------------------------------------------------
+// EFdWrapper ctors
+//----------------------------------------------------------------------------
+inline EFdWrapper::EFdWrapper( int fd ) : mfd (fd)
+{
+	// Create from an open file descriptor
+}
+	
+inline EFdWrapper::EFdWrapper( const string& strFile, int oflag, int pmode )
+	: mfd (-1)
+{
+	// Open the named file
+	Open (strFile, oflag, pmode);
+}
+	
+//----------------------------------------------------------------------------
+// EFdWrapper dtor
+//----------------------------------------------------------------------------
+inline EFdWrapper::~EFdWrapper( )
+{
+	// Cleanup by closing the file
+	Close ();
+}
+	
+//----------------------------------------------------------------------------
+// EFdWrapper::Open( )
+//----------------------------------------------------------------------------
+inline bool EFdWrapper::Open( const string& strFile, int oflag, int pmode )
+{
+	// Close any previous files
+	Close ();
+	
+	// Open the named file
+	if( !strFile.empty() )
+		mfd = open (strFile.c_str(), oflag, pmode);
+	return IsValid ();
+}
+	
+//----------------------------------------------------------------------------
+// EFdWrapper::Close( )
+//----------------------------------------------------------------------------
+inline void EFdWrapper::Close( )
+{
+	// Close the file
+	if( mfd != -1 )
+		//VERIFY assure(close (mfd) == 0);
+		close(mfd);
+	mfd = -1;
+}
+
+#endif 
+
+//eof
