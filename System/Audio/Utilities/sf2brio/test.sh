0398af558dcc4209d0e506e0850d720dadf2358d
gklimanis <gklimanis@leapfrog.com>
2007-11-06 18:53:56 -0800

    updated test file with new options (WAV, AIFF) for sf2brio.
    (SVN Revision 2190 from svn://localhost/TRUNK/)

--- System/Audio/Utilities/sf2brio/test.sh
+++ System/Audio/Utilities/sf2brio/test.sh
@@ -1,12 +1,12 @@
-./sf2brio -v -i belltree_up2.aiff -outFormat brio -o out.brio
-./sf2brio -i out.brio
+#./sf2brio -v -i belltree_up2.aiff -outFormat brio -o out.brio
+#./sf2brio -i out.brio
 
 # No you don't want the padding from 12 to 16 bytes
 #./sf2brio -v -i belltree_up2.aiff -outFormat brio -pad 4 -o outPad4.brio
 #./sf2brio -i outPad4.brio
 
 #./sf2brio -i belltree_up2.aiff -outFormat wav  -o out.wav
-#./sf2brio -i belltree_up2.aiff -outFormat raw  -o out.raw
+./sf2brio -i belltree_up2.aiff -outFormat raw  -o out22.raw
 #./sf2brio -i belltree_up2.aiff -outFormat aiff -o out.aiff
 
 
