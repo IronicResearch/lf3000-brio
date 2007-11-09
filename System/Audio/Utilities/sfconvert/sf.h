967ecbef51e0d35ce06659667fce6ad018c6c1e0
gklimanis <gklimanis@leapfrog.com>
2007-11-09 14:22:54 -0800

    Big MAMA Utility to test DSP functions.
    (SVN Revision 2213 from svn://localhost/TRUNK/)

---
+++ System/Audio/Utilities/sfconvert/sf.h
@@ -0,0 +1,36 @@
+// *************************************************************** 
+// sf.h:	Header file for some Brio functions
+//
+//		Written by Gints Klimanis, 2007
+// ***************************************************************
+
+#ifndef __SF_H__
+#define	__SF_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/* Add whatever here */
+#ifdef __cplusplus
+}
+#endif
+
+// Some types defined for Brio
+typedef unsigned long  U32;
+typedef unsigned short U16;
+typedef                U32 tRsrcType;
+
+// Brio audio file header
+struct tAudioHeader {
+	U32	  offsetToData;		// Offset from the start of the header to
+					// the start of the data (std is 16)
+	tRsrcType type;			// AudioRsrcType
+	U16	  flags;		// Bit mask of audio flags
+					// (Bit0: 0=mono, 1=stereo)
+	U16	 sampleRate;		// Sample rate in Hz			
+	U32	 dataSize;		// Data size in bytes
+};
+
+#endif  //	end __SF_H__
+
+
