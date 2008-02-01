c8c9cbf6ad69ba95d141835ff54070657d694cf6
Dave Milici <dmilici@leapfrog.com>
2008-01-31 20:22:29 -0800

    Revert test changes for video framebuffer location.
    (SVN Revision 2869 from svn://localhost/TRUNK/)

--- System/Display/Include/DisplayPriv.h
+++ System/Display/Include/DisplayPriv.h
@@ -35,7 +35,7 @@ LF_BEGIN_BRIO_NAMESPACE()
 const CString			kDisplayModuleName		= "Display";
 const tVersion			kDisplayModuleVersion	= 2;
 const tEventPriority	kDisplayEventPriority	= 0;
-const tDebugLevel		kDisplayDebugLevel		= kDbgLvlVerbose;
+const tDebugLevel		kDisplayDebugLevel		= kDbgLvlCritical;
 
 //==============================================================================
 // Typedefs
