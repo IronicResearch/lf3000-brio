5797c21d4ab461d869f697df67dc22f806e4c22a
Rob Dowling <rdowling@leapfrog.com>
2008-03-24 16:51:16 -0700

    Make a general patch-set creator, not just for Brio
    (SVN Revision 3194 from svn://localhost/TRUNK/)

--- Etc/make-patch-set.sh
+++ Etc/make-patch-set.sh
@@ -2,59 +2,53 @@
 
 # make-patch-set.sh.
 #
-#    3/20/1008 R Dowling
+#    3/24/2008 R Dowling
+
+# Given two directories: gmpackages and newpackages, make a new directory
+# patchpackages with patch files for each package in newpackages.
 #
-# Make a minimalized BrioPatch-X.lfp from a Brio-X.lfp, by diffing
-# md5sums with a golden Brio-GM.lfp.
-#
-# Clever trick from Dave Milici is to copy new .lfp, then simply
-# delete any files that have the same md5sum as gold copies.
-#
-# Futher cleverness is to used packagefiles.md5 as list of files and
-# md5sums.
+# For each package X-V.lfp in newpackages, make a minimized XPatch-V.lfp
+# by diffing md5sums found in packagefiles.md5 of a golden X-V.lfp.
 
-# Our new Brio LFP
-NewBrioLFP=$1
-if [ "x$NewBrioLFP" = "x" ]; then
-	echo "Usage: ./make-patch-set.sh Brio-X.Y.Z.T.lfp"
-	exit 1
-fi
 
-# Extract a temp copy of the new lfp's packagefiles.md5
-NewBrioMD5=`mktemp`
-unzip -p $NewBrioLFP Brio/packagefiles.md5 > $NewBrioMD5
+# The directories: from, to
+GMDIR=gmpackages
+NEWDIR=newpackages
 
-# The gold Brio
-GMBrioLFP=Brio-0.1.47.3011.lfp
-GMBrioMD5=${GMBrioLFP/.lfp/}-packagefiles.md5
+# Create output directory
+PATCHDIR=patchpackages
+rm -rf $PATCHDIR
+mkdir -p $PATCHDIR
 
-# The directory which Brio unpacks into
-BrioDir=Brio
-
-PatchBrioLFP=${NewBrioLFP/Brio/BrioPatch}
-
-echo "Making minimal patch set $PatchBrioLFP (that takes from $GMBrioLFP)"
-echo "Using $GMBrioMD5 as GM base."
-echo ========================================================================
-
-cp $NewBrioLFP $PatchBrioLFP
-
-# diff -rq $GMBrioDir $NewBrioDir
-for i in `cat $GMBrioMD5 | cut -b 35-`; do
-	gmm5=`grep "$i" $GMBrioMD5 | cut -b 1-33`
-	newm5=`grep "$i" $NewBrioMD5 | cut -b 1-33`
-	if [ "$gmm5" == "$newm5" ]; then
-		# Remove from copy of .lfp
-		echo $gmm5 == $newm5 $i
-		zip -dq $PatchBrioLFP ${i/./$BrioDir}
-	else
-		# Keep
-		echo $gmm5 != $newm5 $i
-	fi
+for n in $NEWDIR/*; do
+	# Examples: 
+	#   n="newpackages/Avatar1-28.lfp"
+	#   g="gmpackages/Avatar1-26.4.lfp"
+	#   p="patchpackages/Avatar1Patch-28.lfp"
+	g1=${n/$NEWDIR/$GMDIR}
+	g2=${g1/-*.lfp/}
+	g=`echo $g2-*.lfp`
+	p1=${n/$NEWDIR/$PATCHDIR}
+	p=${p1/-/Patch-}
+	echo "n=$n g=$g p=$p"
+	# Make a copy of package in patch directory
+	cp -v $g $p
+	# Extract the packagefiles.md5 from g and n packages
+	tg=`mktemp`; unzip -p $g */packagefiles.md5 > $tg
+	tn=`mktemp`; unzip -p $n */packagefiles.md5 > $tn
+	# For each file listed in package
+	for i in `cat $tg | cut -b 35-`; do
+		gm5=`grep "$i" $tg | cut -b 1-33`
+		nm5=`grep "$i" $tn | cut -b 1-33`
+		if [ "$gm5" == "$nm5" ]; then
+			# Remove from copy of .lfp
+			echo $gm5 == $nm5 $i
+			zip -dq $p ${i/./*}
+		else
+			# Keep
+			echo $gm5 != $nm5 $i
+		fi
+	done
+	rm $tg $tp
 done
-
-rm -f $NewBrioMD5
-
-# Show results
-echo ========================================================================
-ls -l $NewBrioLFP $PatchBrioLFP
+exit 0;
