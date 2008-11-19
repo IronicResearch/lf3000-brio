82427c8859ea3b87db2599703e45d13575038750
Scott Esters <sesters@leapfrog.com>
2008-11-19 15:56:44 -0800

    Add support for /flags/vbus flag, allowing developers to debug applications with USB connected.
    (SVN Revision 3704 from svn://localhost/TRUNK/)

--- System/Utility/USBDeviceState.cpp
+++ System/Utility/USBDeviceState.cpp
@@ -26,15 +26,27 @@ tUSBDeviceData GetCurrentUSBDeviceState(void)
 {
 		FILE *usb_device_fd;
 		int ret;
+		U32 usbState;
 		tUSBDeviceData data;
-		data.USBDeviceSupports = kUSBDeviceIsMassStorage;
-		
-		usb_device_fd = fopen("/sys/devices/platform/lf1000-usbgadget/vbus", "r");
-		if(usb_device_fd == NULL)
-			return data;
 
-		ret = fscanf(usb_device_fd, "%ld\n", &data.USBDeviceState);
-		fclose(usb_device_fd);
+		data.USBDeviceSupports = kUSBDeviceIsMassStorage;
+		usbState = 0;
+
+		/* check /flags/vbus development flag, if present */
+		usb_device_fd = fopen("/flags/vbus","r");
+		if (usb_device_fd != NULL) {
+			ret = fscanf(usb_device_fd, "%ld\n", &usbState);
+			fclose(usb_device_fd);
+		
+		} else { /* no /flags/vbus entry, check actual device */
+			usb_device_fd =
+				fopen("/sys/devices/platform/lf1000-usbgadget/vbus", "r");
+			if(usb_device_fd != NULL) {
+				ret = fscanf(usb_device_fd, "%ld\n", &usbState);
+				fclose(usb_device_fd);
+			}
+		}
+		data.USBDeviceState = (usbState != 0) ? kUSBDeviceConnected : 0;
 		return data;
 }
 
