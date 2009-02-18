d79fac6e1c85888c05a873ea4e1f3dd79d675b4f
Minh Saelock <msaelock@leapfrog.com>
2009-02-18 12:55:12 -0800

    Addition of ButtonEventQueue and TouchEventQueue.
    (SVN Revision 3961 from svn://localhost/TRUNK/)

---
+++ System/Include/ButtonEventQueue.h
@@ -0,0 +1,44 @@
+#ifndef LF_BRIO_BUTTON_EVENT_QUEUE_H
+#define LF_BRIO_BUTTON_EVENT_QUEUE_H
+
+//==============================================================================
+// $Source: $
+//
+// Copyright (c) LeapFrog Enterprises, Inc.
+//==============================================================================
+//
+// File:
+//		ButtonEventQueue.h
+//
+// Description:
+//		Defines utility class CButtonEventQueue.
+//		Register this event listener, and it will queue up all the button events.
+//		Regularly use popQueue at a time of convenience to process the events.
+//		Remember to unregister it when you don't want to queue up button events.
+//
+//==============================================================================
+
+#include <EventListener.h>
+#include <KernelMPI.h>
+#include <queue>
+#include <ButtonTypes.h>
+LF_BEGIN_BRIO_NAMESPACE()
+
+//==============================================================================
+class CButtonEventQueue : public IEventListener
+{
+public:
+	CButtonEventQueue();
+	~CButtonEventQueue();
+	
+	std::queue<CButtonMessage> popQueue();
+	
+private:
+	tEventStatus Notify(const IEventMessage &msgIn);
+	CKernelMPI kernel_;
+	tMutex mutex_;
+	std::queue<CButtonMessage> eventQueue_;
+};
+
+LF_END_BRIO_NAMESPACE()	
+#endif //LF_BRIO_BUTTON_EVENT_QUEUE_H
---
+++ System/Include/TouchEventQueue.h
@@ -0,0 +1,44 @@
+#ifndef LF_BRIO_TOUCH_EVENT_QUEUE_H
+#define LF_BRIO_TOUCH_EVENT_QUEUE_H
+
+//==============================================================================
+// $Source: $
+//
+// Copyright (c) LeapFrog Enterprises, Inc.
+//==============================================================================
+//
+// File:
+//		TouchEventQueue.h
+//
+// Description:
+//		Defines utility class CTouchEventQueue.
+//		Register this event listener, and it will queue up all the touch events.
+//		Regularly use popQueue at a time of convenience to process the events.
+//		Remember to unregister it when you don't want to queue up button events.
+//
+//==============================================================================
+
+#include <EventListener.h>
+#include <KernelMPI.h>
+#include <queue>
+#include <TouchTypes.h>
+LF_BEGIN_BRIO_NAMESPACE()
+
+//==============================================================================
+class CTouchEventQueue : public IEventListener
+{
+public:
+	CTouchEventQueue();
+	~CTouchEventQueue();
+	
+	std::queue<CTouchMessage> popQueue();
+	
+private:
+	tEventStatus Notify(const IEventMessage &msgIn);
+	CKernelMPI kernel_;
+	tMutex mutex_;
+	std::queue<CTouchMessage> eventQueue_;
+};
+
+LF_END_BRIO_NAMESPACE()
+#endif //LF_BRIO_TOUCH_EVENT_QUEUE_H
