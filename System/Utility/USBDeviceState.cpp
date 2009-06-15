8ea5cc9aba829a02fe682c46bd7ff4d86e5a8e7a
Dave Milici <dmilici@leapfrog.com>
2009-06-15 12:32:51 -0700

    Remove temporary doom file check and vbus flag overide to return actual USB connected state.
    (SVN Revision 4544 from svn://localhost/TRUNK/)

--- System/Utility/USBDeviceState.cpp
+++ System/Utility/USBDeviceState.cpp
@@ -32,28 +32,12 @@ tUSBDeviceData GetCurrentUSBDeviceState(void)
 		// Emerald is Ethernet device type
 		data.USBDeviceSupports = kUSBDeviceIsEthernet;
 		usbState = 0;
-		
-		// Interim check for DOOM sentinel file since it is permissible 
-		// for AppManager to keep running while USB connected on Emerald
-		struct stat buf;
-		if (stat("/LF/doom", &buf) == 0) {
-			data.USBDeviceState = kUSBDeviceConnected;
-			return data;
-		}
 
-		/* check /flags/vbus development flag, if present */
-		usb_device_fd = fopen("/flags/vbus","r");
-		if (usb_device_fd != NULL) {
+		usb_device_fd =
+			fopen("/sys/devices/platform/lf1000-usbgadget/vbus", "r");
+		if(usb_device_fd != NULL) {
 			ret = fscanf(usb_device_fd, "%ld\n", &usbState);
 			fclose(usb_device_fd);
-		
-		} else { /* no /flags/vbus entry, check actual device */
-			usb_device_fd =
-				fopen("/sys/devices/platform/lf1000-usbgadget/vbus", "r");
-			if(usb_device_fd != NULL) {
-				ret = fscanf(usb_device_fd, "%ld\n", &usbState);
-				fclose(usb_device_fd);
-			}
 		}
 		data.USBDeviceState = (usbState != 0) ? kUSBDeviceConnected : 0;
 		return data;
