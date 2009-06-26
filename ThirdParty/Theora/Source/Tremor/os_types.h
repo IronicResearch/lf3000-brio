a6e4e7ddacce4f2a88e4183de16dd21d669b0703
Dave Milici <dmilici@leapfrog.com>
2009-06-26 16:33:24 -0700

    Interim merge of Vorbis 1.2.0 code into local Tremor tree. (Development will not impact Brio builds until final binaries are checked in once they are working.)
    Most noteworthy finding is that Tremor code was using old Ogg packet structures and functions internally instead of referencing Ogg headers and libs externally. The merge will be successfully completed when able to parse any mixed-mode Ogg packets like Theora instead of handing only Vorbis packets. The fixed-point decoding and quantization routines are isolated in separate code modules and should not need to be updated. 
    
    (SVN Revision 4694 from svn://localhost/TRUNK/)

--- ThirdParty/Theora/Source/Tremor/os_types.h
+++ ThirdParty/Theora/Source/Tremor/os_types.h
@@ -14,8 +14,8 @@
  function: #ifdef jail to whip a few platforms into the UNIX ideal.
 
  ********************************************************************/
-#ifndef _OS_TYPES_H
-#define _OS_TYPES_H
+#ifndef _OS_TYPES2_H
+#define _OS_TYPES2_H
 
 #ifdef _LOW_ACCURACY_
 #  define X(n) (((((n)>>22)+1)>>1) - ((((n)>>22)+1)>>9))
@@ -25,64 +25,6 @@
 #  define LOOKUP_T const ogg_int32_t
 #endif
 
-/* make it easy on the folks that want to compile the libs with a
-   different malloc than stdlib */
-#define _ogg_malloc  malloc
-#define _ogg_calloc  calloc
-#define _ogg_realloc realloc
-#define _ogg_free    free
+#include <ogg/os_types.h>
 
-#ifdef _WIN32 
-
-#  ifndef __GNUC__
-   /* MSVC/Borland */
-   typedef __int64 ogg_int64_t;
-   typedef __int32 ogg_int32_t;
-   typedef unsigned __int32 ogg_uint32_t;
-   typedef __int16 ogg_int16_t;
-#  else
-   /* Cygwin */
-   #include <_G_config.h>
-   typedef _G_int64_t ogg_int64_t;
-   typedef _G_int32_t ogg_int32_t;
-   typedef _G_uint32_t ogg_uint32_t;
-   typedef _G_int16_t ogg_int16_t;
-#  endif
-
-#elif defined(__MACOS__)
-
-#  include <sys/types.h>
-   typedef SInt16 ogg_int16_t;
-   typedef SInt32 ogg_int32_t;
-   typedef UInt32 ogg_uint32_t;
-   typedef SInt64 ogg_int64_t;
-
-#elif (defined(__APPLE__) && defined(__MACH__)) /* MacOS X Framework build */
-
-#  include <sys/types.h>
-   typedef int16_t ogg_int16_t;
-   typedef int32_t ogg_int32_t;
-   typedef u_int32_t ogg_uint32_t;
-   typedef int64_t ogg_int64_t;
-
-#elif defined(__BEOS__)
-
-   /* Be */
-#  include <inttypes.h>
-
-#elif defined (__EMX__)
-
-   /* OS/2 GCC */
-   typedef short ogg_int16_t;
-   typedef int ogg_int32_t;
-   typedef unsigned int ogg_uint32_t;
-   typedef long long ogg_int64_t;
-
-#else
-
-#  include <sys/types.h>
-#  include "config_types.h"
-
-#endif
-
-#endif  /* _OS_TYPES_H */
+#endif  /* _OS_TYPES2_H */
