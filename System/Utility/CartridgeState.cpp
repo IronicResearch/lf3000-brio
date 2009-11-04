e7e12044d7c2ca0f0fa687e8ecc073b3f76bc015
ytu <ytu@leapfrog.com>
2009-11-03 17:42:23 -0800

    Cart handling moved to CartManager process, Cart thread here only handles message from CartManager
    (SVN Revision 5752 from svn://localhost/TRUNK/)

--- System/Utility/CartridgeState.cpp
+++ System/Utility/CartridgeState.cpp
@@ -2,15 +2,35 @@
 #include <Utility.h>
 #include <DebugMPI.h>
 
+#define CART_BRIO_STATE  "/tmp/cart_brio_state"	
 
 LF_BEGIN_BRIO_NAMESPACE()
 
-static tCartridgeData gCartridgeData;
+static tCartridgeData gCartridgeData = {CARTRIDGE_STATE_CLEAN};
 
 tCartridgeData GetCartridgeState(void)
 {
-	tCartridgeData data = gCartridgeData;
+	#if 0
+	return gCartridgeData;
+	#else
+	int ret;
+	FILE *fp;
+	tCartridgeData data = {CARTRIDGE_STATE_CLEAN};
+	CDebugMPI debug(kGroupCartridge);
+	
+
+	fp = fopen(CART_BRIO_STATE, "r");
+	if(fp == NULL) {
+		// debug.DebugOut(kDbgLvlImportant, "can't open %s for read !\n", CART_BRIO_STATE);
+		return data;
+	}
+	ret = fscanf(fp, "%d", (int *)&data);
+	if((ret == 0) || (ret == EOF)) {
+		debug.DebugOut(kDbgLvlCritical, "data at %s: Format Wrong \n", CART_BRIO_STATE);
+	} 
+	fclose(fp);
 	return data;
+	#endif
 }
 
 /* match defines in CartridgeTypes.h */
@@ -28,8 +48,6 @@ static const char *cartridgeStateName[] = {
 	"CARTRIDGE_STATE_UNKNOWN"
 };
 
-#define CART_BRIO_STATE  "/tmp/cart_brio_state"	
-
 void SetCartridgeState(tCartridgeData cartridge_data)
 {
 	FILE *fp;
@@ -42,7 +60,7 @@ void SetCartridgeState(tCartridgeData cartridge_data)
 		debug.DebugOut(kDbgLvlImportant, "can't access %s !\n", CART_BRIO_STATE);
 		return;
 	}
-	if(fprintf(fp, "%s\n", cartridgeStateName[cartridge_data.cartridgeState]) < 0) {
+	if(fprintf(fp, "%d, %s\n", cartridge_data.cartridgeState, cartridgeStateName[cartridge_data.cartridgeState]) < 0) {
 		debug.DebugOut(kDbgLvlImportant, "can't write to %s !\n", CART_BRIO_STATE);
 	}
 	fclose(fp);
