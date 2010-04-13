e85eee531f076e96e719c7dfc9c239df2323630e
dlazzari <dlazzari@leapfrog.com>
2010-04-13 12:07:52 -0700

    - Changes to build scripts to run with scons 1.2.0 - Minor improvements in running unit tests in emulation - Added unit test run path for embedded testing. If an embedded build is run with runtests=t and a unit is connected in developer mode, the unit tests will run. - All unit test results are now placed in the Build/Lightning/test_results/ or Build/Lightning_emulation/test_results/ directories depending on the build type.
    (SVN Revision 7052 from svn://localhost/TRUNK/)

--- Etc/Tools/SConsTools/runtest.py
+++ Etc/Tools/SConsTools/runtest.py
@@ -5,26 +5,83 @@
 #-----------------------------------------------------------------------------
 import SCons.Builder
 import os
+import subprocess
+import DeviceTools.telnetrpc
 
 #-----------------------------------------------------------------------------
 # Add the tool(s) to the construction environment object
 #-----------------------------------------------------------------------------
 def generate(env):
-    
-    def builder_unit_test(target, source, env):
-		# TODO: BC: This fails for embedded target because test can't be run.
-        app = str(source[0].abspath)
-        if os.spawnl(os.P_WAIT, app, app)==0:
-            open(str(target[0]),'w').write("PASSED\n")
-        else:
-            return 1
 
-    bld = SCons.Builder.Builder(action = builder_unit_test)
-    env['BUILDERS']['RunTest'] = bld
+	def builder_unit_test(target, source, env):
+		#Emulation unit test
+		if env['type'] == 'emulation':
+			#Run through each test given
+			for t, s in zip(target, source):
+				app = str(s.abspath)
+				failed_cases = subprocess.call( [ app ], shell=False)
+				if failed_cases == 0:
+					results = open(str(t), 'w')
+					results.write("PASSED\n")
+					results.close()
+				else:
+					results = open(str(t), 'w')
+					results.write("FAILED " + str(failed_cases) + " cases\n")
+					results.close()
+		
+		#Embedded unit test
+		elif env['type'] == 'embedded':
+			#Connect to device and mount nfsroot's LF
+			mount_cmd = "mount -o nolock `get-ip host`:" + env['vars']['rootfs'] + " /LF"
+			try:
+				rpc = DeviceTools.telnetrpc.TelnetRPC("192.168.0.111", "root")
+			except Exception:
+				for t in target:
+					results_log = open(str(t), 'w')
+					results_log.write("BLOCKED\n\nFailed to connect to device")
+					results_log.close()
+					
+				raise
+			
+			rpc.command(mount_cmd, verbose=True)
+			
+			#Run tests
+			#TODO: This way of calculating paths is not very dynamic and elegant
+			for t, s in zip(target, source):
+				device_test_path = "/LF" + str(s.abspath) [ len( env['vars']['rootfs'] ) : ]
+				print("Running: " + device_test_path)
+				
+				result = rpc.command(device_test_path)
+				failed_cases = rpc.command("echo $?")
+				
+				print("Test Complete")
+				
+				#Log results
+				try:
+					results_log = open(str(t), 'w')
+				except Exception:
+					print("Failed to open results log")
+					raise
+				
+				#Check fail/pass
+				if failed_cases[0] == "0":
+					results_log.write("PASSED\n\n")
+				else:
+					results_log.write("FAILED " + failed_cases[0] + " cases\n\n")
+				results_log.write("Detailed Log:\n")
+				for line in result:
+					results_log.write(line + "\n")
+				results_log.close()
+			
+			rpc.command("umount -lf /LF")
+			rpc.close()
+
+	bld = SCons.Builder.Builder(action = builder_unit_test)
+	env['BUILDERS']['RunTest'] = bld
 
 
 #-----------------------------------------------------------------------------
 # Report the presence of the tool
 #-----------------------------------------------------------------------------
 def exists(env):
-     return 1
+	return 1
