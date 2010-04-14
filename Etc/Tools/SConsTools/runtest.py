827fcf9f2544a034146e54392468fa4d0eba47bf
dlazzari <dlazzari@leapfrog.com>
2010-04-14 14:48:32 -0700

    - Changed module build scripts to defer checking the is_runtests flag to the RunMyTests function for easier maintenance. - Added a 5 minute watchdog timer to emulation unit tests to stop them if they hang - Changed type=publish builds to default to building and running embedded unit tests, to run emulation unit tests as well, use runtests=t with type=publish - If embedded unit tests are set to run, reboot any connected device before building and testing
    (SVN Revision 7081 from svn://localhost/TRUNK/)

--- Etc/Tools/SConsTools/runtest.py
+++ Etc/Tools/SConsTools/runtest.py
@@ -6,7 +6,14 @@
 import SCons.Builder
 import os
 import subprocess
+import signal
 import DeviceTools.telnetrpc
+
+class TestTimeoutError(Exception):
+	pass
+
+def TestTimeoutHandler(signum, frame):
+	raise TestTimeoutError
 
 #-----------------------------------------------------------------------------
 # Add the tool(s) to the construction environment object
@@ -15,22 +22,45 @@ def generate(env):
 
 	def builder_unit_test(target, source, env):
 		#Emulation unit test
-		if env['type'] == 'emulation':
+		if env['vars']['is_emulation']:
 			#Run through each test given
 			for t, s in zip(target, source):
 				app = str(s.abspath)
-				failed_cases = subprocess.call( [ app ], shell=False)
+				
+				#Startup test and create a timer
+				result = []
+				test_process = subprocess.Popen( [ app ], shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+				signal.signal(signal.SIGALRM, TestTimeoutHandler)
+				signal.alarm(300)
+				
+				#Read lines from the pipe until the test finishes or times out
+				try:
+					while (test_process.returncode == None):
+						result.append( test_process.stdout.readline() )
+						if result[-1] == "":
+							break
+						print(result[-1])
+					signal.alarm(0)
+				except TestTimeoutError:
+					appname = os.path.basename(app)
+					os.system( "killall " + str(appname) )
+					result.append("Test Terminated Due To Timeout")
+					test_process.wait()
+					
+				#Collect result data
+				failed_cases = test_process.returncode
+				results_log = open(str(t), 'w')
 				if failed_cases == 0:
-					results = open(str(t), 'w')
-					results.write("PASSED\n")
-					results.close()
+					results_log.write("PASSED\n\n")
 				else:
-					results = open(str(t), 'w')
-					results.write("FAILED " + str(failed_cases) + " cases\n")
-					results.close()
+					results_log.write("FAILED " + str(failed_cases) + " cases\n\n")
+				results_log.write("Detailed Log:\n")
+				for line in result:
+					results_log.write( line )
+				results_log.close()
 		
 		#Embedded unit test
-		elif env['type'] == 'embedded':
+		else:
 			#Connect to device and mount nfsroot's LF
 			mount_cmd = "mount -o nolock `get-ip host`:" + env['vars']['rootfs'] + " /LF"
 			try:
@@ -40,29 +70,37 @@ def generate(env):
 					results_log = open(str(t), 'w')
 					results_log.write("BLOCKED\n\nFailed to connect to device")
 					results_log.close()
-					
-				raise
+				return
 			
-			rpc.command(mount_cmd, verbose=True)
+			try:
+				rpc.command(mount_cmd, verbose=True)
+			except Exception:
+				for t in target:
+					results_log = open(str(t), 'w')
+					results_log.write("BLOCKED\n\nFailed to mount nfsroot")
+					results_log.close()
+				return
 			
 			#Run tests
 			#TODO: This way of calculating paths is not very dynamic and elegant
 			for t, s in zip(target, source):
-				device_test_path = "/LF" + str(s.abspath) [ len( env['vars']['rootfs'] ) : ]
-				print("Running: " + device_test_path)
-				
-				result = rpc.command(device_test_path)
-				failed_cases = rpc.command("echo $?")
-				
-				print("Test Complete")
-				
 				#Log results
 				try:
 					results_log = open(str(t), 'w')
 				except Exception:
 					print("Failed to open results log")
-					raise
+					continue
 				
+				device_test_path = "/LF" + str(s.abspath) [ len( env['vars']['rootfs'] ) : ]
+				
+				try:
+					result = rpc.command(device_test_path)
+					failed_cases = rpc.command("echo $?")
+				except Exception:
+					results_log.write("BLOCKED\n\nError in connection to device")
+					results_log.close()
+					continue
+					
 				#Check fail/pass
 				if failed_cases[0] == "0":
 					results_log.write("PASSED\n\n")
@@ -73,8 +111,11 @@ def generate(env):
 					results_log.write(line + "\n")
 				results_log.close()
 			
-			rpc.command("umount -lf /LF")
-			rpc.close()
+			try:
+				rpc.command("umount -lf /LF")
+				rpc.close()
+			except Exception:
+				return
 
 	bld = SCons.Builder.Builder(action = builder_unit_test)
 	env['BUILDERS']['RunTest'] = bld
