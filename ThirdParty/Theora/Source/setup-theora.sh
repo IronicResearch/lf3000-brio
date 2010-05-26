51643e1f12a7ff67ecaed13abdc34e5fe01f9cdf
ayurovsky <ayurovsky@leapfrog.com>
2010-05-26 12:39:47 -0700

    theora: rework setup script, give option for IDCT support
    (SVN Revision 7481 from svn://localhost/TRUNK/)

--- ThirdParty/Theora/Source/setup-theora.sh
+++ ThirdParty/Theora/Source/setup-theora.sh
@@ -3,7 +3,7 @@
 # Build theora libs from source available via SVN.
 THEORA_LIB_VER=libtheora-1.0
 THEORA_LIB_SRC=$THEORA_LIB_VER.tar.gz
-THEORA_LIB_DIR=Theora
+THEORA_LIB_DIR=$THEORA_LIB_VER
 
 set -e
 
@@ -27,15 +27,20 @@ fi
 if [ "$CLEAN" == "1" -o ! -e $THEORA_LIB_DIR ]; then
 	rm -rf $THEORA_LIB_DIR
 	tar -xzf $THEORA_LIB_SRC
-	cp -R $THEORA_LIB_VER $THEORA_LIB_DIR
 fi
 
 # build and copy shared libs to rootfs
 pushd $THEORA_LIB_DIR
 ./configure --host=arm-linux --build=x86-linux --prefix=${ROOTFS_PATH}/usr/local --exec-prefix=${ROOTFS_PATH}/usr/local --enable-shared=yes --disable-float --disable-encode --disable-examples --disable-oggtest --disable-vorbistest --disable-sdltest --with-ogg=${ROOTFS_PATH}/usr/local --with-ogg-includes=${ROOTFS_PATH}/usr/local/include --with-ogg-libraries=${ROOTFS_PATH}/usr/local/lib 
-set +e
-patch -N -p1 <../Theora-Makefile.patch
-set -e
+
+# use the LF1000 IDCT instead of the software IDCT
+if [ "x$USE_IDCT" != "x" ]; then
+	mkdir -p lib/dec/arm
+	cp ../state_lf1000.c lib/dec/arm/
+	cp ../idct_lf1000.c lib/dec/arm/
+	patch -p1 < ../Theora-use-idct.patch
+fi
+
 make
 make install
 cp -a ./lib/.libs/libtheora.so* ../../Libs/arm/ 
