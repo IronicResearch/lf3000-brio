baf372c01ca17a19bfdab0da52d30161c5429e80
Dave Milici <dmilici@leapfrog.com>
2010-10-01 11:43:22 -0700

    DisplayMPI: Interim code for framebuffer driver. (Not active by default.) Support YUV vs RGB framebuffers.
    (SVN Revision 8501 from svn://localhost/TRUNK/)

--- System/Display/Lightning/DisplayFB.cpp
+++ System/Display/Lightning/DisplayFB.cpp
@@ -23,14 +23,17 @@
 
 LF_BEGIN_BRIO_NAMESPACE()
 
+#define RGBFB					0	// index of RGB framebuffer
+#define YUVFB					2	// index of YUV framebuffer
+#define NUMFB					3	// number of framebuffer devices
+
 namespace 
 {
-	const char*					FBDEV = "/dev/fb0";
-	const char*					FBYUV = "/dev/fb2";
-	int 						fbdev = -1;
-	struct fb_fix_screeninfo 	finfo;
-	struct fb_var_screeninfo 	vinfo;
-	U8*							fbmem = 0;
+	const char*					FBDEV[NUMFB] = {"/dev/fb0", "/dev/fb1", "/dev/fb2"};
+	int 						fbdev[NUMFB] = {-1, -1, -1};
+	struct fb_fix_screeninfo 	finfo[NUMFB];
+	struct fb_var_screeninfo 	vinfo[NUMFB];
+	U8*							fbmem[NUMFB] = {NULL, NULL, NULL};
 	int 						index = 0; // FIXME -- just for testing
 }
 
@@ -41,42 +44,48 @@ namespace
 void CDisplayFB::InitModule()
 {
 	int r;
-	
-	// Open framebuffer device
-	fbdev = open(FBDEV, O_RDWR | O_SYNC);
-	dbg_.Assert(fbdev >= 0, "%s: Error opening %s\n", __FUNCTION__, FBDEV);
-	
-	// Query framebuffer info
-	r = ioctl(fbdev, FBIOGET_FSCREENINFO, &finfo);
-	dbg_.Assert(r == 0, "%s: Error querying %s\n", __FUNCTION__, FBDEV);
-	
-	r = ioctl(fbdev, FBIOGET_VSCREENINFO, &vinfo);
-	dbg_.Assert(r == 0, "%s: Error querying %s\n", __FUNCTION__, FBDEV);
-	dbg_.DebugOut(kDbgLvlImportant, "%s: Screen = %d x %d\n", __FUNCTION__, vinfo.xres, vinfo.yres);
 
-	// Map framebuffer into userspace
-	fbmem = (U8*)mmap(0, finfo.smem_len, PROT_READ | PROT_WRITE, MAP_SHARED, fbdev, 0);
-	dbg_.Assert(fbmem != MAP_FAILED, "%s: Error mapping %s\n", __FUNCTION__, FBDEV);
-	dbg_.DebugOut(kDbgLvlImportant, "%s: Mapped %08lx to %p, size %u\n", __FUNCTION__, finfo.smem_start, fbmem, finfo.smem_len);
+	for (int n = 0; n < NUMFB; n++)
+	{
+		// Open framebuffer device
+		fbdev[n] = open(FBDEV[n], O_RDWR | O_SYNC);
+		dbg_.Assert(fbdev[n] >= 0, "%s: Error opening %s\n", __FUNCTION__, FBDEV[n]);
+		
+		// Query framebuffer info
+		r = ioctl(fbdev[n], FBIOGET_FSCREENINFO, &finfo[n]);
+		dbg_.Assert(r == 0, "%s: Error querying %s\n", __FUNCTION__, FBDEV[n]);
+		
+		r = ioctl(fbdev[n], FBIOGET_VSCREENINFO, &vinfo[n]);
+		dbg_.Assert(r == 0, "%s: Error querying %s\n", __FUNCTION__, FBDEV[n]);
+		dbg_.DebugOut(kDbgLvlImportant, "%s: Screen = %d x %d\n", __FUNCTION__, vinfo[n].xres, vinfo[n].yres);
+	
+		// Reset page flip offsets
+		vinfo[n].xoffset = vinfo[n].yoffset = 0;
+		r = ioctl(fbdev[n], FBIOPUT_VSCREENINFO, &vinfo[n]);
+		
+		// Map framebuffer into userspace
+		fbmem[n] = (U8*)mmap(0, finfo[n].smem_len, PROT_READ | PROT_WRITE, MAP_SHARED, fbdev[n], 0);
+		dbg_.Assert(fbmem[n] != MAP_FAILED, "%s: Error mapping %s\n", __FUNCTION__, FBDEV[n]);
+		dbg_.DebugOut(kDbgLvlImportant, "%s: Mapped %08lx to %p, size %u\n", __FUNCTION__, finfo[n].smem_start, fbmem[n], finfo[n].smem_len);
+	}
 }
 
 //----------------------------------------------------------------------------
 void CDisplayFB::DeInitModule()
 {
-	// FIXME: Framebuffer driver remembers page offset???
-	vinfo.yoffset = 0;
-	ioctl(fbdev, FBIOPUT_VSCREENINFO, &vinfo);
-	
-	// Release framebuffer mapping and device
-	munmap(fbmem, finfo.smem_len);
-	
-	close(fbdev);
+	for (int n = 0; n < NUMFB; n++)
+	{
+		// Release framebuffer mapping and device
+		munmap(fbmem[n], finfo[n].smem_len);
+		
+		close(fbdev[n]);
+	}
 }
 
 //----------------------------------------------------------------------------
 U32	CDisplayFB::GetScreenSize()
 {
-	return (vinfo.yres << 16) | (vinfo.xres);
+	return (vinfo[RGBFB].yres << 16) | (vinfo[RGBFB].xres);
 }
 
 //----------------------------------------------------------------------------
@@ -91,24 +100,28 @@ tDisplayHandle CDisplayFB::CreateHandle(U16 height, U16 width, tPixelFormat colo
 	tDisplayContext* ctx = new tDisplayContext;
 
 	int depth;
+	int n, r;
 	switch (colorDepth) 
 	{
-		case kPixelFormatRGB4444:	depth = 16; break;
-		case kPixelFormatRGB565:	depth = 16; break;
-		case kPixelFormatRGB888:	depth = 24; break; 	
+		case kPixelFormatRGB4444:	depth = 16; n = RGBFB; break;
+		case kPixelFormatRGB565:	depth = 16; n = RGBFB; break;
+		case kPixelFormatRGB888:	depth = 24; n = RGBFB; break; 	
 		default:
-		case kPixelFormatARGB8888: 	depth = 32; break;
-		case kPixelFormatYUV420:	depth = 8 ; break;
-		case kPixelFormatYUYV422:	depth = 16; break;
+		case kPixelFormatARGB8888: 	depth = 32; n = RGBFB; break;
+		case kPixelFormatYUV420:	depth = 8 ; n = YUVFB; break;
+		case kPixelFormatYUYV422:	depth = 16; n = YUVFB; break;
 	}		
 
 	// FIXME: Framebuffer driver doesn't look at pixel format???
-	vinfo.bits_per_pixel = depth;
-	int r = ioctl(fbdev, FBIOPUT_VSCREENINFO, &vinfo);
-	r = ioctl(fbdev, FBIOGET_VSCREENINFO, &vinfo);
-	r = ioctl(fbdev, FBIOGET_FSCREENINFO, &finfo);
+	if (n == RGBFB)
+	{
+		vinfo[n].bits_per_pixel = depth;
+		r = ioctl(fbdev[n], FBIOPUT_VSCREENINFO, &vinfo[n]);
+		r = ioctl(fbdev[n], FBIOGET_VSCREENINFO, &vinfo[n]);
+		r = ioctl(fbdev[n], FBIOGET_FSCREENINFO, &finfo[n]);
+	}
 	
-	int offset = index * vinfo.yres * finfo.line_length;
+	int offset = index * vinfo[n].yres * finfo[n].line_length;
 	index++;
 	
 	memset(ctx, 0, sizeof(tDisplayContext));
@@ -117,10 +130,17 @@ tDisplayHandle CDisplayFB::CreateHandle(U16 height, U16 width, tPixelFormat colo
 	ctx->colorDepthFormat 	= colorDepth;
 	ctx->depth				= depth;
 	ctx->bpp				= depth/8;
-	ctx->pitch				= finfo.line_length;
+	ctx->pitch				= finfo[n].line_length;
 	ctx->isAllocated		= (pBuffer != NULL);
-	ctx->pBuffer			= (pBuffer != NULL) ? pBuffer : fbmem + offset;
+	ctx->pBuffer			= (pBuffer != NULL) ? pBuffer : fbmem[n] + offset;
 	ctx->offset 			= offset;
+	ctx->layer				= n;
+	ctx->isOverlay			= (n == YUVFB);
+	ctx->isPlanar			= (finfo[n].type == FB_TYPE_PLANES);
+
+	// FIXME: YUV planar pitch is reported incorrectly
+	if (ctx->isPlanar)
+		ctx->pitch = 4096;
 	
 	return (tDisplayHandle)ctx;
 }
@@ -146,7 +166,8 @@ tErrType CDisplayFB::RegisterLayer(tDisplayHandle hndl, S16 xPos, S16 yPos)
 	ctx->rect.right		= xPos + ctx->width;
 	ctx->rect.bottom 	= yPos + ctx->height;
 	
-	int r = ioctl(fbdev, FBIOBLANK, 0);
+	int n = ctx->layer;
+	int r = ioctl(fbdev[n], FBIOBLANK, 0);
 	
 	return (r == 0) ? kNoErr : kNoImplErr;
 }
@@ -156,7 +177,8 @@ tErrType CDisplayFB::UnRegisterLayer(tDisplayHandle hndl)
 {
 	tDisplayContext* ctx = (tDisplayContext*)hndl;
 
-	int r = ioctl(fbdev, FBIOBLANK, 1);
+	int n = ctx->layer;
+	int r = ioctl(fbdev[n], FBIOBLANK, 1);
 	
 	return (r == 0) ? kNoErr : kNoImplErr;
 }
@@ -171,9 +193,12 @@ tErrType CDisplayFB::Update(tDisplayContext* dc, int sx, int sy, int dx, int dy,
 tErrType CDisplayFB::SwapBuffers(tDisplayHandle hndl, Boolean waitVSync)
 {
 	tDisplayContext* ctx = (tDisplayContext*)hndl;
+	int n = ctx->layer;
 	
-	vinfo.yoffset = ctx->offset / finfo.line_length; 
-	int r = ioctl(fbdev, FBIOPAN_DISPLAY, &vinfo);
+	// Note pages are stacked vertically for RGB, horizontally for YUV
+	vinfo[n].yoffset = ctx->offset / finfo[n].line_length;
+	vinfo[n].xoffset = ctx->offset % finfo[n].line_length;
+	int r = ioctl(fbdev[n], FBIOPAN_DISPLAY, &vinfo[n]);
 
 	pdcVisible_->flippedContext = ctx;
 	
@@ -183,6 +208,13 @@ tErrType CDisplayFB::SwapBuffers(tDisplayHandle hndl, Boolean waitVSync)
 //----------------------------------------------------------------------------
 Boolean	CDisplayFB::IsBufferSwapped(tDisplayHandle hndl)
 {
+	tDisplayContext* ctx = (tDisplayContext*)hndl;
+	
+	// FIXME: VBLANK info not implemented?
+	struct fb_vblank vblank;
+	int n = ctx->layer;
+	int r = ioctl(fbdev[n], FBIOGET_VBLANK, &vblank);
+	
 	return true; // FIXME
 }
 
