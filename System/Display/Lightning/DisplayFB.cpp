1e1872e74cf05e0725bac36a8da1942daed7e689
Minh Saelock <msaelock@leapfrog.com>
2011-05-17 11:05:43 -0700

    TTPro Brio 1208.  Updating ViewPort variables during CreateHandle.  Prevents issues with launching a GameViewFramed Tutorial from a non-GameViewFramed Game.
    (SVN Revision 9313 from svn://localhost/TRUNK/)

--- System/Display/Lightning/DisplayFB.cpp
+++ System/Display/Lightning/DisplayFB.cpp
@@ -255,6 +255,8 @@ tDisplayHandle CDisplayFB::CreateHandle(U16 height, U16 width, tPixelFormat colo
 	if (colorDepth == kPixelFormatRGB565 && pBuffer == pmem2d)
 		n = OGLFB;
 
+	// Update dxres && dyres in case someone called SetViewPort without calling GetScreenStats
+	pModule_->GetViewport(pModule_->GetCurrentDisplayHandle(), dxres, dyres, vxres, vyres);
 	// FIXME: RGB lower layer needed when viewport active
 	// FIXME: Switch RGB layer prior to isUnderlay setting
 	if (n == RGBFB && (dxres > 0 || dyres > 0))
