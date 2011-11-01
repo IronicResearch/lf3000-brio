8af3dd5f5a9b3b4c2a78086ed6293f14a79fd7a7
jeno <jeno@leapfrog.com>
2011-10-31 17:21:13 -0700

    Unit tests for VIP hardware.
    (SVN Revision 9539 from svn://localhost/BRANCHES/Brio_LF2000/)

---
+++ System/Camera/tests/TestVIP.h
@@ -0,0 +1,239 @@
+// TestCameraMPI.h
+
+#include <cxxtest/TestSuite.h>
+#include <SystemErrors.h>
+#include <StringTypes.h>
+#include <CameraMPI.h>
+#include <DisplayMPI.h>
+#include <KernelMPI.h>
+#include <EventMPI.h>
+#include <VideoMPI.h>
+#include <EventListener.h>
+#include <AudioTypes.h>
+#include <UnitTestUtils.h>
+
+#include <sstream>
+
+//For strcmp
+#include <string.h>
+
+#include "Utility.h"
+
+
+LF_USING_BRIO_NAMESPACE()
+
+const tDebugSignature kMyApp = kTestSuiteDebugSig;
+const tEventType LocalCameraEvents[] = {kAllCameraEvents};
+
+const char* capture_path = "/LF/Bulk/Data/Local/All/";
+
+//============================================================================
+// TestCameraMPI functions
+//============================================================================
+class TestCamera : public CxxTest::TestSuite, TestSuiteBase
+{
+private:
+	CCameraMPI*		pCameraMPI_;
+	CDisplayMPI*		pDisplayMPI_;
+	CKernelMPI*		pKernelMPI_;
+	CVideoMPI*		pVideoMPI_;
+	tEventType		reason;
+
+	//============================================================================
+	// Local event listener
+	//============================================================================
+	class CameraListener : public IEventListener
+	{
+	private:
+		tEventType	reason;
+		U32			length;
+	public:
+		CameraListener():
+			IEventListener(LocalCameraEvents, ArrayCount(LocalCameraEvents))
+			{reason = kUndefinedEventType;}
+
+		tEventStatus Notify(const IEventMessage& Imsg)
+		{
+			const CCameraEventMessage& msg = dynamic_cast<const CCameraEventMessage&>(Imsg);
+
+			reason = Imsg.GetEventType();
+
+			if(reason == kCaptureTimeOutEvent)
+			{
+				length = msg.data.timeOut.length;
+			}
+			if(reason == kCaptureQuotaHitEvent)
+			{
+				length = msg.data.quotaHit.length;
+			}
+			if(reason == kCaptureStoppedEvent)
+			{
+				length = msg.data.stopped.length;
+			}
+			if(reason == kCameraRemovedEvent)
+			{
+				length = msg.data.removed.length;
+			}
+
+			return kEventStatusOK;
+		}
+
+		tEventStatus Reset()
+		{
+			reason = kUndefinedEventType;
+			length = 0;
+		}
+
+		tEventType GetReason()
+		{
+			return reason;
+		}
+
+		U32 GetLength()
+		{
+			return length;
+		}
+	};
+
+public:
+	//------------------------------------------------------------------------
+	void setUp( )
+	{
+		pCameraMPI_ = new CCameraMPI;
+	}
+
+	//------------------------------------------------------------------------
+	void tearDown( )
+	{
+		delete pCameraMPI_;
+	}
+
+	//------------------------------------------------------------------------
+	void testWasCreated( )
+	{
+		PRINT_TEST_NAME();
+		TS_ASSERT( pCameraMPI_ != NULL );
+		TS_ASSERT( pCameraMPI_->IsValid() == true );
+	}
+
+	//------------------------------------------------------------------------
+	void testCoreMPI( )
+	{
+		PRINT_TEST_NAME();
+		tVersion		version;
+		const CString*	pName;
+		const CURI*		pURI;
+
+		tPlatformCaps test = kCapsCamera;
+		TS_ASSERT(HasPlatformCapability(test));
+
+		if ( pCameraMPI_->IsValid() ) {
+			pName = pCameraMPI_->GetMPIName();
+			TS_ASSERT_EQUALS( *pName, "CameraMPI" );
+			version = pCameraMPI_->GetModuleVersion();
+			pName = pCameraMPI_->GetModuleName();
+			printf("Module: %s, version: %d\n", pName->c_str(), version);
+			pURI = pCameraMPI_->GetModuleOrigin();
+			TS_ASSERT_EQUALS( *pURI, "/LF/System/Camera" );
+		}
+
+	}
+
+	//------------------------------------------------------------------------
+	void testOverlay()
+	{
+		PRINT_TEST_NAME();
+
+		tVidCapHndl					capture;
+		Boolean						bRet;
+		tErrType					err;
+
+		// For displaying captured data
+		tVideoSurf				surf;
+		tDisplayHandle			disp;
+
+		pKernelMPI_ = new CKernelMPI;
+		pDisplayMPI_ = new CDisplayMPI;
+		pVideoMPI_ = new CVideoMPI;
+
+		disp = pDisplayMPI_->CreateHandle(300, 400, kPixelFormatYUV420, NULL);
+		TS_ASSERT( disp != kInvalidDisplayHandle );
+		pDisplayMPI_->Register(disp, 0, 0, kDisplayOnTop, 0);
+
+		surf.width = pDisplayMPI_->GetWidth(disp);
+		surf.pitch = pDisplayMPI_->GetPitch(disp);
+		surf.height = pDisplayMPI_->GetHeight(disp);
+		surf.buffer = pDisplayMPI_->GetBuffer(disp);
+		surf.format = pDisplayMPI_->GetPixelFormat(disp);
+		TS_ASSERT( surf.format == kPixelFormatYUV420 );
+
+		if ( pCameraMPI_->IsValid() )
+		{
+			capture = pCameraMPI_->StartVideoCapture(&surf, NULL, "", 0, false);
+			TS_ASSERT_DIFFERS( capture, kInvalidVidCapHndl );
+
+			pKernelMPI_->TaskSleep(5000);
+
+			bRet = pCameraMPI_->StopVideoCapture(capture);
+			TS_ASSERT_EQUALS( bRet, true );
+		}
+		else
+			TS_FAIL("MPI was deemed invalid");
+
+		pDisplayMPI_->UnRegister(disp, 0);
+		pDisplayMPI_->DestroyHandle(disp, true);
+		delete pVideoMPI_;
+		delete pDisplayMPI_;
+		delete pKernelMPI_;
+	}
+
+	//------------------------------------------------------------------------
+	void testCapture()
+	{
+		PRINT_TEST_NAME();
+
+		tVidCapHndl					capture;
+		Boolean						bRet;
+		tErrType					err;
+
+		// For displaying captured data
+		tVideoSurf				surf;
+		tDisplayHandle			disp;
+
+		pKernelMPI_ = new CKernelMPI;
+		pDisplayMPI_ = new CDisplayMPI;
+		pVideoMPI_ = new CVideoMPI;
+
+		disp = pDisplayMPI_->CreateHandle(300, 400, kPixelFormatYUV420, NULL);
+		TS_ASSERT( disp != kInvalidDisplayHandle );
+		pDisplayMPI_->Register(disp, 0, 0, kDisplayOnTop, 0);
+
+		surf.width = pDisplayMPI_->GetWidth(disp);
+		surf.pitch = pDisplayMPI_->GetPitch(disp);
+		surf.height = pDisplayMPI_->GetHeight(disp);
+		surf.buffer = pDisplayMPI_->GetBuffer(disp);
+		surf.format = pDisplayMPI_->GetPixelFormat(disp);
+		TS_ASSERT( surf.format == kPixelFormatYUV420 );
+
+		if ( pCameraMPI_->IsValid() )
+		{
+			capture = pCameraMPI_->StartVideoCapture(NULL/*&surf*/, NULL, "VIP.avi", 0, false);
+			TS_ASSERT_DIFFERS( capture, kInvalidVidCapHndl );
+
+			pKernelMPI_->TaskSleep(5000);
+
+			bRet = pCameraMPI_->StopVideoCapture(capture);
+			TS_ASSERT_EQUALS( bRet, true );
+		}
+		else
+			TS_FAIL("MPI was deemed invalid");
+
+		pDisplayMPI_->UnRegister(disp, 0);
+		pDisplayMPI_->DestroyHandle(disp, true);
+		delete pVideoMPI_;
+		delete pDisplayMPI_;
+		delete pKernelMPI_;
+	}
+};
+
+// EOF
