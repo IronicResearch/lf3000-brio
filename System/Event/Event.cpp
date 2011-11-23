0d9e4b031c3591d46ec21ad008f71d9baa1228cb
Dave Milici <dmilici@leapfrog.com>
2011-11-23 11:50:23 -0800

    EventMPI: Discard stray messages if posted immediately (prior process?) to avoid segfaults.
    (SVN Revision 9610 from svn://localhost/BRANCHES/Brio_LF2000/)

--- System/Event/Event.cpp
+++ System/Event/Event.cpp
@@ -134,6 +134,7 @@ private:
 		pThis->debug_.DebugOut(kDbgLvlVerbose, "EventDispatchTask() Message queue created %d\n", 
 						static_cast<int>(g_hMsgQueueBG_));
 		CEventDispatchMessage	msg;										//*2
+		U32 start = pThis->kernel_.GetElapsedTimeAsMSecs() + 10;
 
 		g_threadRunning_ = true;
 		while (g_threadRun_)
@@ -141,7 +142,7 @@ private:
 			// use timeout for cancellation point
 			err = pThis->kernel_.ReceiveMessageOrWait(g_hMsgQueueBG_, &msg, kEventDispatchMessageSize, 100);
 	        pthread_testcancel();
-			if ( err != kConnectionTimedOutErr && g_threadRun_ ) {
+			if ( err != kConnectionTimedOutErr && g_threadRun_ && pThis->kernel_.GetElapsedTimeAsMSecs() > start) {
 		    	pThis->debug_.AssertNoErr(err, "EventDispatchTask(): Receive message!\n" );
 		    	pThis->PostEventImpl(*(msg.pMsg), msg.pResponse);
 				pThis->kernel_.Free((void *)msg.pMsg);
