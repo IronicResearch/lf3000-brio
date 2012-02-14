70ad95496fd3c5fa38506ef0ba81d6375cda8d52
mdeshpande <mdeshpande@leapfrog.com>
2012-02-13 19:29:58 -0800

    image quality and image format are not optional parameters now. instead added overloaded functions which are used internally. this was needed for external apps to work as is.
    (SVN Revision 9751 from svn://localhost/BRANCHES/Brio_LF2000/)

--- System/ImageIO/JPEG.cpp
+++ System/ImageIO/JPEG.cpp
@@ -57,6 +57,57 @@ bool JPEG_checkFile(FILE* infile, const CPath& path) {
 
 }
 
+bool JPEG_Save(CPath& path, tVideoSurf& surf)
+{
+	int width = surf.width;
+	int height = surf.height;
+	int bytes_per_pixel = surf.pitch / surf.width;
+	//int color_space = JCS_RGB;
+
+	U8* raw_image = surf.buffer;
+
+	struct jpeg_compress_struct cinfo;
+	struct jpeg_error_mgr jerr;
+
+	/* this is a pointer to one row of image data */
+	JSAMPROW row_pointer[1];
+
+	FILE *outfile = fopenAtomic( path.c_str(), "wb" );
+
+	if ( !outfile )
+	{
+		printf("Error opening output jpeg file %s\n!",  path.c_str() );
+		return false;
+	}
+
+	cinfo.err = jpeg_std_error( &jerr );
+	jpeg_create_compress(&cinfo);
+	jpeg_stdio_dest(&cinfo, outfile);
+
+	/* Setting the parameters of the output file here */
+	cinfo.image_width = width;
+	cinfo.image_height = height;
+	cinfo.input_components = bytes_per_pixel;
+	cinfo.in_color_space = JCS_RGB;
+	/* default compression parameters, we shouldn't be worried about these */
+	jpeg_set_defaults( &cinfo );
+
+	/* Now do the compression .. */
+	jpeg_start_compress( &cinfo, TRUE );
+	/* like reading a file, this time write one row at a time */
+	while( cinfo.next_scanline < cinfo.image_height )
+	{
+		row_pointer[0] = &raw_image[ cinfo.next_scanline * cinfo.image_width *  cinfo.input_components];
+		jpeg_write_scanlines( &cinfo, row_pointer, 1 );
+	}
+	/* similar to read file, clean up after we're done compressing */
+	jpeg_finish_compress( &cinfo );
+	jpeg_destroy_compress( &cinfo );
+	fcloseAtomic( outfile );
+	/* success code is 1! */
+	return true;
+}
+
 bool JPEG_Save(CPath& path, tVideoSurf& surf, int imageQuality)
 {
 	int width = surf.width;
