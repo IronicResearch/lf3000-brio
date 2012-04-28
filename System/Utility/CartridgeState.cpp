1e2508a9d5adfff43fbcfbdd81e119dd0b361487
dlazzari <dlazzari@leapfrog.com>
2012-04-27 17:07:22 -0700

    Remove saving cartridge state to file as that is now handled by cnotify. Also clarify initializing cartridge state in CartridgeMPI. Partial fix for TTPro Brio - Base #3276.
    (SVN Revision 9837 from svn://localhost/BRANCHES/Brio_LF2000/)

--- System/Utility/CartridgeState.cpp
+++ System/Utility/CartridgeState.cpp
@@ -33,37 +33,10 @@ tCartridgeData GetCartridgeState(void)
 	#endif
 }
 
-/* match defines in CartridgeTypes.h */
-static const char *cartridgeStateName[] = {
-	"CARTRIDGE_STATE_NONE",
-	"CARTRIDGE_STATE_INSERTED",
-	"CARTRIDGE_STATE_DRIVER_READY",	/* Internal state: driver is fully initialized */
-	"CARTRIDGE_STATE_READY",
-	"CARTRIDGE_STATE_REMOVED",
-	"CARTRIDGE_STATE_FS_CLEAN",		/* Internal state: File system is fully unmounted */
-	"CARTRIDGE_STATE_CLEAN",
-	"CARTRIDGE_STATE_REINSERT",
-	"CARTRIDGE_STATE_RESTART_APPMANAGER",
-	"CARTRIDGE_STATE_REBOOT",
-	"CARTRIDGE_STATE_UNKNOWN"
-};
-
 void SetCartridgeState(tCartridgeData cartridge_data)
 {
-	FILE *fp;
 	gCartridgeData = cartridge_data;
-	CDebugMPI debug(kGroupEvent);
-	
 
-	fp = fopen(CART_BRIO_STATE, "w");
-	if(fp == NULL) {
-		debug.DebugOut(kDbgLvlImportant, "can't access %s !\n", CART_BRIO_STATE);
-		return;
-	}
-	if(fprintf(fp, "%d, %s\n", cartridge_data.cartridgeState, cartridgeStateName[cartridge_data.cartridgeState]) < 0) {
-		debug.DebugOut(kDbgLvlImportant, "can't write to %s !\n", CART_BRIO_STATE);
-	}
-	fclose(fp);
 	return;
 }
 
