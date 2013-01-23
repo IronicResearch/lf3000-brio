4bb21e0f40f9bc854db2b43cafabb3e4c44f6f23
Dave Milici <dmilici@leapfrog.com>
2013-01-23 12:00:48 -0800

    Added sysfs queries to PowerMPI.GetPowerParam() calls.
    (SVN Revision 10095 from svn://localhost/BRANCHES/Brio_LF2000/)

--- System/Power/PublicMPI/PowerMPI.cpp
+++ System/Power/PublicMPI/PowerMPI.cpp
@@ -33,6 +33,15 @@ const CString	kMPIName = "PowerMPI";
 
 static int		gTimeoutMSec = 5000;
 
+const CString	SYSFS_POWER_LF1000			= "/sys/devices/platform/lf1000-power/";
+const CString	SYSFS_POWER_LF2000			= "/sys/devices/platform/lf2000-power/";
+static CString	SYSFS_POWER_ROOT			= SYSFS_POWER_LF2000;
+
+inline const char* SYSFS_POWER_PATH(const char* path)
+{
+	return CString(SYSFS_POWER_ROOT + path).c_str();
+}
+
 //============================================================================
 // CPowerMessage
 //============================================================================
@@ -180,6 +189,59 @@ int CPowerMPI::Reset() const
 //----------------------------------------------------------------------------
 S32	CPowerMPI::GetPowerParam(tPowerParam param)
 {
+	FILE* fd;
+	int value = 0;
+
+	switch (param) {
+	case kPowerParamBatteryMV:
+		fd = fopen(SYSFS_POWER_PATH("voltage"),"r");
+		if (fd) {
+			fscanf(fd, "%d", &value);
+			fclose(fd);
+			return value;
+		}
+		break;
+	case kPowerParamBatteryGuage:
+		fd = fopen("/tmp/battery_level","r");
+		if (fd) {
+			fscanf(fd, "%d", &value);
+			fclose(fd);
+			return value;
+		}
+		break;
+	case kPowerParamMaxBatteryMV:
+		fd = fopen(SYSFS_POWER_PATH("max_battery_mv"),"r");
+		if (fd) {
+			fscanf(fd, "%d", &value);
+			fclose(fd);
+			return value;
+		}
+		break;
+	case kPowerParamNormalBatteryMV:
+		fd = fopen(SYSFS_POWER_PATH("normal_battery_mv"),"r");
+		if (fd) {
+			fscanf(fd, "%d", &value);
+			fclose(fd);
+			return value;
+		}
+		break;
+	case kPowerParamLowBatteryMV:
+		fd = fopen(SYSFS_POWER_PATH("low_battery_mv"),"r");
+		if (fd) {
+			fscanf(fd, "%d", &value);
+			fclose(fd);
+			return value;
+		}
+		break;
+	case kPowerParamCriticalBatteryMV:
+		fd = fopen(SYSFS_POWER_PATH("critical_battery_mv"),"r");
+		if (fd) {
+			fscanf(fd, "%d", &value);
+			fclose(fd);
+			return value;
+		}
+		break;
+	}
 	return 0;
 }
 
