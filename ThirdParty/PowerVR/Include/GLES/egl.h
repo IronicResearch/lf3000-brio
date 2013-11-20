2e0c1851a5a1027337d9827e0e92ceb0c94243a3
Minh Saelock <msaelock@leapfrog.com>
2013-11-20 13:40:06 -0800

    Adding Headers and Libs from PowerVR 3.2 SDK.  This is for getting OpenGL ES 2.0 working in Emulation.
    (SVN Revision 10365 from svn://localhost/BRANCHES/Brio_LF2000/)

--- ThirdParty/PowerVR/Include/GLES/egl.h
+++ ThirdParty/PowerVR/Include/GLES/egl.h
@@ -2,6 +2,8 @@
  * Skeleton egl.h to provide compatibility for early GLES 1.0
  * applications. Several early implementations included gl.h
  * in egl.h leading applications to include only egl.h
+ *
+ * $Revision: 6252 $ on $Date:: 2008-08-06 16:35:08 -0700 #$
  */
 
 #ifndef __legacy_egl_h_
