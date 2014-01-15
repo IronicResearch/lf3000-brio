de733763751d9873c2c00763fe039a1c33171909
dlazzari <dlazzari@leapfrog.com>
2014-01-15 14:17:53 -0800

    Add support for building an LF3000 platform. Currently it won't build because of some ifdef issues.
    (SVN Revision 10453 from svn://localhost/BRANCHES/Brio_LF2000/)

---
+++ Etc/Tools/SConsTools/LF3000_emulation.py
@@ -0,0 +1,51 @@
+#-----------------------------------------------------------------------------
+# Setup the Lightning emulation gcc tool chain
+#-----------------------------------------------------------------------------
+import os.path
+import re
+
+import SCons.Defaults
+import SCons.Tool
+import SCons.Util
+import SCons.Node
+
+common = __import__(__name__.replace('emulation', 'common'), globals(), locals(), [''])
+
+#-----------------------------------------------------------------------------
+# This is the primary function in which to modify global settings for the
+# platform.  The rest of the file is boilerplate.
+#-----------------------------------------------------------------------------
+def PlatformMods(env):
+	common.SetPlatformFlags(env)
+	common.SetPlatformIncludePaths(env)
+	common.SetPlatformLibraryPaths(env)
+	
+	gcc_defs 			= env.Split('')
+	emulation_defs		= env.Split('EMULATION')
+	env.Append(CPPDEFINES = gcc_defs + emulation_defs)
+#	env.Append(CCFLAGS = '-g -Wextra')
+	env.Append(CCFLAGS = '-g' )
+	env.Append(LINKFLAGS = '-g')
+	env.ParseConfig('pkg-config --cflags --libs glib-2.0')
+	env.ParseConfig('pkg-config --cflags --libs glibmm-2.4')
+
+	
+#-----------------------------------------------------------------------------
+# Inherit properties from the following tools
+#-----------------------------------------------------------------------------
+parent = __import__('SCons.Tool.g++', globals(), locals(), [''])
+
+#-----------------------------------------------------------------------------
+# Add the tool(s) to the construction environment object
+#-----------------------------------------------------------------------------
+def generate(env):
+	"""NOTE: We can default to the standard Linux g++ toolchain here, and only
+	override specific compiler and linker flags."""
+	PlatformMods(env)
+
+
+#-----------------------------------------------------------------------------
+# Report the presence of the tool(s)
+#-----------------------------------------------------------------------------
+def exists(env):
+	return parent.exists(env)
