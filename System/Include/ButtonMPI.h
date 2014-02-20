f1eb1483d7aac71622c127915a38ea18d904a2a1
Carlo Castaneda <ccastaneda@leapfrog.com>
2014-02-20 14:46:17 -0800

    ==================================== r10513 | cmiller | 2014-02-18 14:37:38 -0800 (Tue, 18 Feb 2014) | 1 line Changed paths:    M /BRANCHES/Flakasoft_SDK/System/Include/ButtonMPI.h
    First pass at documenting ButtonMPI.h
    ====================================
    
    
    (SVN Revision 10527 from svn://localhost/BRANCHES/Brio_LF2000/)

--- System/Include/ButtonMPI.h
+++ System/Include/ButtonMPI.h
@@ -10,8 +10,7 @@
 //		ButtonMPI.h
 //
 // Description:
-//		Defines the Module Public Interface (MPI) for the Button Manager module. 
-//
+//		Defines the Module Public Interface (MPI) for the Button Manager module.
 //==============================================================================
 
 #include <CoreMPI.h>
@@ -26,41 +25,42 @@ LF_BEGIN_BRIO_NAMESPACE()
 class CButtonMPI : public ICoreMPI {
 	/// \class CButtonMPI
 	///
-	/// Button manager class for posting CButtonMessage events on button 
-	/// presses or releases to registered listeners of kGroupButton type. 
-	/// Button on/off states and transitions are bitwise encoded in 
-	/// tButtonData fields, with time-stamp info logged to additional 
+	/// Button manager class for posting CButtonMessage events on button
+	/// presses or releases to registered listeners of kGroupButton type.
+	/// Button on/off states and transitions are bitwise encoded in
+	/// tButtonData fields, with time-stamp info logged to additional
 	/// tButtonData2 fields.
-public:	
+public:
 	// ICoreMPI functionality
 	virtual	Boolean			IsValid() const;
-	virtual const CString*	GetMPIName() const;		
+	virtual const CString*	GetMPIName() const;
 	virtual tVersion		GetModuleVersion() const;
-	virtual const CString*	GetModuleName() const;	
+	virtual const CString*	GetModuleName() const;
 	virtual const CURI*		GetModuleOrigin() const;
 
 	// class-specific functionality
 	CButtonMPI();
 	virtual ~CButtonMPI();
-	
-	/// register handler
+
+	/// Register event listener via CEventMPI and enables continuous sampling mode.
 	tErrType RegisterEventListener(const IEventListener *pListener);
-	/// unregister handler
+	/// Unregister event listener via CEventMPI and disables all sampling.
 	tErrType UnregisterEventListener(const IEventListener *pListener);
-	
-	/// Get button state
+
+	/// Get button state (up or down) and transition.
 	tButtonData		GetButtonState() const;
+	/// Get button state (up or down), transition and a time stamp for the transition.
 	tButtonData2	GetButtonState2() const;
 
-	/// Get touch state
+	/// Get touch state (touch or no touch) and the x, y co-ordinates.
 	tTouchData		GetTouchState() const;
 
-	/// Get touch sample rate (Hz)
+	/// Get touch sample rate (Hz). This shows how often the touch state is checked.
 	U32				GetTouchRate() const;
 
-	/// Set touch sample rate (Hz)
+	/// Set touch sample rate (Hz). This sets how often the touch state is checked.
 	/// Values are set to the nearest value of 50Hz/1, 50Hz/2, 50Hz/3, etc.
-	/// The values that possible are:
+	/// Possible values are:
 	/// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 20, 25, 33, 50]
 	tErrType		SetTouchRate(U32 rate);
 
@@ -69,14 +69,19 @@ public:
 
 	/// Set touch mode (enum tTouchMode)
 	tErrType		SetTouchMode(tTouchMode mode);
-	
+
 	/// Get touch parameter value (enum tTouchParam)
 	U32				GetTouchParam(tTouchParam param) const;
 
 	/// Set touch parameter value (enum tTouchParam)
 	tErrType		SetTouchParam(tTouchParam param, U32 value);
-	
+
+    /// Gets the orientation that events use to interpret the DPad buttons,
+    /// either landscape (Kdpadlandscape), or portrait(Kdpadportrait).
 	tDpadOrientation	GetDpadOrientation();
+
+	/// Sets the orientation that events use to interpret the DPad buttons,
+    /// either landscape (Kdpadlandscape), or portrait(Kdpadportrait).
 	tErrType			SetDpadOrientation(tDpadOrientation dpad_orientation);
 
 private:
@@ -84,7 +89,7 @@ private:
 };
 
 
-LF_END_BRIO_NAMESPACE()	
+LF_END_BRIO_NAMESPACE()
 #endif // LF_BRIO_BUTTONMPI_H
 
 // eof
