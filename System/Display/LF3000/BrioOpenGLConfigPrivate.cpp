d48e5c00ee030103855c1da4cbd9205d4bdb6507
Minh Saelock <msaelock@leapfrog.com>
2014-02-26 13:02:28 -0800

    For LF3000, uncommenting the new glEnableSpecialMode calls, they are included in the last angstrom-denzil toolchain.  Added commented glSetSpecialModeParam that isn't yet in the toolchain.
    (SVN Revision 10542 from svn://localhost/BRANCHES/Brio_LF2000/)

--- System/Display/LF3000/BrioOpenGLConfigPrivate.cpp
+++ System/Display/LF3000/BrioOpenGLConfigPrivate.cpp
@@ -342,8 +342,9 @@ void BrioOpenGLConfigPrivate::Init(enum tBrioOpenGLVersion brioOpenGLVersion)
 
 	surface_context_map[eglSurface] = &brioopenglconfig_context_map[this];
 	// Clear garbage pixels from previous OpenGL context (embedded target)
-	//glEnableSpecialMode(GL_SPECIAL_MODE_LAST_TEXTURE_EN);
-	//glEnableSpecialMode(GL_SPECIAL_MODE_PIXEL_FOG_EN);
+	glEnableSpecialMode(GL_SPECIAL_MODE_LAST_TEXTURE_EN);
+	glEnableSpecialMode(GL_SPECIAL_MODE_PIXEL_FOG_EN);
+	//glSetSpecialModeParam(GL_SPECIAL_MODE_PIXEL_FOG_EN, 10000);
 
 	glClearColorx(0, 0, 0, 0);
 	glClear(GL_COLOR_BUFFER_BIT);
