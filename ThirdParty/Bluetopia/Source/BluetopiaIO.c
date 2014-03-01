c03528bd122a76ddc506d461be425dc1d6dc0360
Dave Milici <dmilici@leapfrog.com>
2014-02-28 19:17:09 -0800

    Created dynamic library with stub functions for testing Bluetopia client. (FWSCRUM-1405)
    (SVN Revision 10552 from svn://localhost/BRANCHES/Brio_LF2000/)

---
+++ ThirdParty/Bluetopia/Source/BluetopiaIO.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int BTIO_Init(void* callback);
+int BTIO_Exit(int handle);
+int BTIO_SendCommand(int handle, int command, void* data, int length);
+int BTIO_QueryStatus(int handle, int command, void* data, int length);
+
+int BTIO_Init(void* callback)
+{
+	printf("%s: %p\n", __func__, callback);
+	return 0;
+}
+
+int BTIO_Exit(int handle)
+{
+	printf("%s: %d\n", __func__, handle);
+	return 0;
+}
+
+int BTIO_SendCommand(int handle, int command, void* data, int length)
+{
+	printf("%s: %d: %d, %p, %d\n", __func__, handle, command, data, length);
+	return 0;
+}
+
+int BTIO_QueryStatus(int handle, int command, void* data, int length)
+{
+	printf("%s: %d: %d, %p, %d\n", __func__, handle, command, data, length);
+	return 0;
+}
