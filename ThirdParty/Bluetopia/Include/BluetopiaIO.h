6e978229e1bca4fe948ce7f3c5165a8e476282b1
Lloyd Moore <lmoore@leapfrog.com>
2014-04-21 09:41:30 -0700

    Exposed DEVM_EnableBluetoothDebug and DEVM_DeleteRemoteDevice in libBluetopiaIO.so (FWGLAS-379)
    (SVN Revision 10754 from svn://localhost/BRANCHES/Brio_LF2000/)

--- ThirdParty/Bluetopia/Include/BluetopiaIO.h
+++ ThirdParty/Bluetopia/Include/BluetopiaIO.h
@@ -14,6 +14,8 @@ typedef int (*pFnScanForDevices)( int, int );
 typedef int (*pFnConnectToDevice)( int, const BTAddr* );
 typedef BTAddr* (*pFnGetLocalAddress)( int ); 
 typedef int (*pFnGetControllerVersion)( const char*, unsigned char*, unsigned short*);
+typedef int (*pFnDeleteRemoteDevice)( const char *);
+typedef int (*pFnEnableBluetoothDebug)( bool, unsigned int, unsigned long, const char*);
 
 typedef void (*pFnCallback)( void*, void*, int );
 typedef void (*pFnCallback2)( void*, void*, int, char* );
@@ -28,6 +30,8 @@ extern pFnScanForDevices		pBTIO_ScanForDevices;
 extern pFnConnectToDevice		pBTIO_ConnectToDevice;
 extern pFnGetLocalAddress		pBTIO_GetLocalAddress;
 extern pFnGetControllerVersion	pBTIO_GetControllerVersion;
+extern pFnDeleteRemoteDevice	pBTIO_DeleteRemoteDevice;
+extern pFnEnableBluetoothDebug	pBTIO_EnableBluetoothDebug;
 
 #define BTIO_Init   			pBTIO_Init
 #define BTIO_Exit   			pBTIO_Exit
@@ -37,6 +41,8 @@ extern pFnGetControllerVersion	pBTIO_GetControllerVersion;
 #define BTIO_ConnectToDevice		pBTIO_ConnectToDevice
 #define BTIO_GetLocalAddress		pBTIO_GetLocalAddress
 #define BTIO_GetControllerVersion	pBTIO_GetControllerVersion
+#define BTIO_DeleteRemoteDevice		pBTIO_DeleteRemoteDevice
+#define BTIO_EnableBluetoothDebug	pBTIO_EnableBluetoothDebug
 
 #else
 
@@ -50,6 +56,8 @@ int BTIO_ScanForDevices(int handle, int scan_time);
 int BTIO_ConnectToDevice(int handle, const BTAddr* device);
 BTAddr* BTIO_GetLocalAddress(int handle);
 int BTIO_GetControllerVersion(const char* device, unsigned char* hwVersion, unsigned short* fwVersion);
+int BTIO_DeleteRemoteDevice(const char *device);
+int BTIO_EnableBluetoothDebug(bool enable, unsigned int type, unsigned long flags, const char *filename);
 
 };
 
