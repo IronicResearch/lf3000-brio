4a3996b062ba46f478f07033e68d38e97a3dc056
mharris <mharris@leapfrog.com>
2014-08-15 12:47:23 -0700

    Vision unit test for VNDurationTrigger, plus production code changes Set/Get, range check. (FWGLAS-1011)
    (SVN Revision 11014 from svn://localhost/BRANCHES/Brio_LF2000/)

--- System/Vision/VNDurationTriggerPIMPL.cpp
+++ System/Vision/VNDurationTriggerPIMPL.cpp
@@ -7,10 +7,26 @@ namespace Vision {
     duration_(duration),
     wasSpatiallyTriggered_(false) {
 
+	SetDuration(duration);
   }
 
   VNDurationTriggerPIMPL::~VNDurationTriggerPIMPL(void) {
 
+  }
+
+  float
+  VNDurationTriggerPIMPL::GetDuration(void) const {
+	  return duration_;
+  }
+
+  void
+  VNDurationTriggerPIMPL::SetDuration(float duration) {
+
+	if (duration >= 0.0f) {
+	  duration_ = duration;
+	} else {
+	  duration_ = 0.0f;
+	}
   }
 
   bool
